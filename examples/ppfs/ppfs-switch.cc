12895b7b80bc614a09e3ab0b32c6b18874694f07
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2017-05-10 16:51:13 +0200

    Add functionality to insert an entry in the switch's routing table
    Add the required functionality to add an entry into a switch's routing
    table.

--- examples/ppfs/ppfs-switch.cc
+++ examples/ppfs/ppfs-switch.cc
@@ -1,6 +1,8 @@
 #include "ns3/abort.h"
 #include "ppfs-switch.h"
 
+using namespace ns3;
+
 PpfsSwitch::PpfsSwitch ()
 {}
 
@@ -14,4 +16,31 @@ PpfsSwitch::InsertNetDevice(uint32_t linkId, ns3::Ptr<ns3::NetDevice> device)
   NS_ABORT_MSG_IF(ret.second == false,
                   "The Link ID " << linkId << " is already stored in node's "
                   << m_id << " Link->NetDevice map");
+}
+
+void
+PpfsSwitch::InsertEntryInRoutingTable(uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
+                                      char protocol, uint32_t linkId, double flowRatio)
+{
+  FlowMatch currentFlow (srcIpAddr, dstIpAddr, portNumber, protocol);
+
+  auto ret = m_linkNetDeviceTable.find(linkId);
+  NS_ABORT_MSG_IF(ret == m_linkNetDeviceTable.end(), "Link " << linkId << " was not found in "<<
+                  "the routing table of Switch with Id " << m_id);
+  Ptr<NetDevice> forwardDevice = ret->second;
+
+  auto routingTableEntry = m_routingTable.find(currentFlow);
+  if (routingTableEntry == m_routingTable.end()) // Route does not exist in table
+    {
+      std::vector<ForwardingAction> forwardAction;
+      forwardAction.push_back(ForwardingAction(forwardDevice, flowRatio));
+      m_routingTable.insert({currentFlow, forwardAction});
+    }
+  else // Route already exists in table
+    {
+      std::vector<ForwardingAction>& forwardActions = routingTableEntry->second;
+      ForwardingAction& lastAction = forwardActions.back();
+      double currentSplitRatio = lastAction.splitRatio + flowRatio;
+      forwardActions.push_back(ForwardingAction(forwardDevice, currentSplitRatio));
+    }
 }
--- examples/ppfs/ppfs-switch.h
+++ examples/ppfs/ppfs-switch.h
@@ -11,13 +11,62 @@ public:
   PpfsSwitch ();
   PpfsSwitch (uint32_t id);
   void InsertNetDevice (uint32_t linkId, ns3::Ptr<ns3::NetDevice> device);
+  void InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
+                                  char protocol, uint32_t linkId, double flowRatio);
 private:
+  struct FlowMatch
+  {
+    uint32_t srcIpAddr;
+    uint32_t dstIpAddr;
+    uint16_t portNumber; // Destination Port Number
+    enum Protocol { Tcp = 'T', Udp = 'U' };
+    Protocol protocol;
+
+    FlowMatch (uint32_t srcIpAddr, uint32_t dstIpAddr,
+               uint16_t port, char protocol) : srcIpAddr (srcIpAddr), dstIpAddr (dstIpAddr),
+                                                   portNumber (port)
+    {
+      if (protocol == 'T')
+        this->protocol = Protocol::Tcp;
+      if (protocol == 'U')
+        this->protocol = Protocol::Udp;
+    }
+
+    bool operator<(const FlowMatch &other) const
+    {
+      /*
+       * Used by the map to store the keys in order.
+       * In this case it is sorted by Source Ip Address, then Destination Ip Address, and
+       * finally Port Number.
+       */
+      if (srcIpAddr == other.srcIpAddr)
+        {
+          if (dstIpAddr == other.dstIpAddr)
+            return portNumber < other.portNumber;
+          else
+            return dstIpAddr < other.dstIpAddr;
+        }
+      else
+        return srcIpAddr < other.srcIpAddr;
+    }
+  };
+
+  struct ForwardingAction
+  {
+    ns3::Ptr<ns3::NetDevice> port;
+    double splitRatio;
+
+    ForwardingAction (ns3::Ptr<ns3::NetDevice> port, double splitRatio) : port (port),
+                                                                          splitRatio (splitRatio)
+    {}
+  };
   /*
    * Key -> Link Id, Value -> Pointer to the net device connected to that link.
    * This map will be used for transmission and when building the routing table.
    */
   std::map <uint32_t, ns3::Ptr<ns3::NetDevice>> m_linkNetDeviceTable;
-
+  // Key -> FlowMatch, Value -> Vector of Forwarding actions
+  std::map <FlowMatch, std::vector<ForwardingAction>> m_routingTable;
   uint32_t m_id; /*!< Stores the node's id */
 };
 
