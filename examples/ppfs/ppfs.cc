8b016729b67389c3bca9af23836652648aabb4e5
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2017-05-10 16:52:26 +0200

    Add a simple hard-coded application to test packet reception

--- examples/ppfs/ppfs.cc
+++ examples/ppfs/ppfs.cc
@@ -27,6 +27,7 @@
 
 #include "ppfs-switch.h"
 #include "topology-builder.h"
+#include "routing-helper.h"
 
 using namespace ns3;
 using namespace tinyxml2;
@@ -46,10 +47,46 @@ main (int argc, char *argv[])
   NS_ABORT_MSG_IF(rootNode == nullptr, "No root node node found");
 
   std::map<uint32_t, PpfsSwitch> switchMap; /*!< Key -> Node ID. Value -> Switch object */
-  NodeContainer allNodes;
-  TopologyBuilder topologyBuilder (rootNode, switchMap, allNodes);
+  std::map <uint32_t, LinkInformation> linkInformation; /*!< Key -> Link ID, Value -> Link Info */
+  NodeContainer allNodes; /*!< Node container storing all the nodes */
+  NodeContainer terminalNodes; /*!< Node container storing a reference to the terminal nodes */
+  NodeContainer switchNodes; /*!< Node container storing a reference to the switch nodes */
+  TopologyBuilder topologyBuilder (rootNode, switchMap, allNodes, terminalNodes, switchNodes);
 
   topologyBuilder.CreateNodes ();
-  topologyBuilder.BuildNetworkTopology ();
+  topologyBuilder.ParseNodeConfiguration();
+  topologyBuilder.BuildNetworkTopology (linkInformation);
+  topologyBuilder.AssignIpToTerminals ();
+
+  RoutingHelper routingHelper;
+  routingHelper.PopulateRoutingTables(switchMap, linkInformation, allNodes, rootNode);
+  routingHelper.SetReceiveFunctionForSwitches(switchNodes);
+
+  // Simple test application!
+  ns3::Ptr<Ipv4> nodeIp = allNodes.Get(8)->GetObject<Ipv4> (); // Get Ipv4 instance of the node
+  InetSocketAddress sinkSocket (nodeIp->GetAddress(1, 0).GetBroadcast(), 9);
+  OnOffHelper onOff ("ns3::UdpSocketFactory", sinkSocket);
+  onOff.SetAttribute ("OnTime",  StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  onOff.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  onOff.SetAttribute ("PacketSize", UintegerValue (1470));
+  onOff.SetAttribute ("DataRate", DataRateValue(5000000)); // Data rate is set to 5Mbps
+  onOff.SetAttribute("MaxBytes", UintegerValue (1470)); // 1 packet
+
+  ApplicationContainer srcApp = onOff.Install(allNodes.Get(0));
+  srcApp.Start(Seconds (1));
+  srcApp.Stop(Seconds (10));
+
+  // Configuring receiver
+  PacketSinkHelper sinkHelper ("ns3::UdpSocketFactory", sinkSocket);
+  ApplicationContainer sinkApp = sinkHelper.Install(allNodes.Get(8));
+  sinkApp.Start(Seconds (0));
+  sinkApp.Stop(Seconds (10));
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  // How to retrieve IP address of node
+  //ns3::Ptr<Ipv4> nodeIp = allNodes.Get(0)->GetObject<Ipv4> (); // Get Ipv4 instance of the node
+  //std::cout << nodeIp->GetAddress (1, 0).GetLocal ().Get() << std::endl;
+
   return 0;
 }
