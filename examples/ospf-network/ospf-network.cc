3ad5e7a651c20d9ca8ae54b12078a271b6e1cabe
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2017-08-24 15:22:08 +0200

    Set the TCP buffer size to 15MB for high speed transmission
    The send/receive TCP window size was increased to 15MB to enable it to reach the gigabit data rate.

--- examples/ospf-network/ospf-network.cc
+++ examples/ospf-network/ospf-network.cc
@@ -93,6 +93,13 @@ main (int argc, char *argv[])
   RandomGeneratorManager::SetSeed(seed);
   RandomGeneratorManager::SetRun(initRun);
 
+  // Setting the default TCP segment size and socket type
+  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (1446));
+  Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TcpNewReno::GetTypeId()));
+  // 15 MB of TCP Transmit and Receive buffer
+  Config::SetDefault ("ns3::TcpSocket::RcvBufSize", UintegerValue (15728640));
+  Config::SetDefault ("ns3::TcpSocket::SndBufSize", UintegerValue (15728640));
+
   // Parsing the XML file.
   XMLDocument xmlLogFile;
   XMLError error = xmlLogFile.LoadFile(xmlLogFilePath.c_str());
