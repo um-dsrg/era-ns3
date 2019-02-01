0601ad66fc508157aa1c11f4c37c6d76b6117df5
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-01 09:01:05 +0100

    Move node creation code to the header file
    TopologyBuilder is a template class therefore the implementation needs to be in the header file. Previously, the topology-builder.cc file was being included in the header file. This made for obscure code that wasn't obvious. Therefore the implementation code is now being moved to the header file *below* the class definition.

--- common-code/topology-builder.h
+++ common-code/topology-builder.h
@@ -13,6 +13,8 @@
 
 #include "definitions.h"
 
+using namespace ns3;
+
 template <class SwitchType>
 class TopologyBuilder
 {
@@ -49,20 +51,95 @@ private:
   // ns3::NodeContainer &m_terminalNodes;
   // ns3::NodeContainer &m_switchNodes;
   std::map<id_t, SwitchType> m_switches; //!< Maps the node id with the Switch object.
-  std::map<id_t, ns3::Ptr<ns3::Node>> m_terminals; //!< Maps the node id with the ns3 node.
+  std::map<id_t, Ptr<Node>> m_terminals; //!< Maps the node id with the ns3 node.
   /* Refactoring - END */
 
-  std::vector<ns3::NetDeviceContainer> m_linkNetDeviceContainers;
+  std::vector<NetDeviceContainer> m_linkNetDeviceContainers;
   /*
-   * When set to true, the tow net devices that make one whole link will be stored in the vector
-   * m_linkNetDevicePair. This information will be used to assign IP addresses in the OSPF scenario
-   * because each link must have a net "network".
-   */
+     * When set to true, the tow net devices that make one whole link will be stored in the vector
+     * m_linkNetDevicePair. This information will be used to assign IP addresses in the OSPF scenario
+     * because each link must have a net "network".
+     */
   bool m_storeNetDeviceLinkPairs;
 
-  ns3::NS_LOG_TEMPLATE_DECLARE; //!< Logging
+  NS_LOG_TEMPLATE_DECLARE; //!< Logging
 };
 
+// #include "topology-builder.cc"
+
+/**
+   * Implementation of the TopologyBuilder class
+   */
+
+template <class SwitchType>
+TopologyBuilder<SwitchType>::TopologyBuilder () : NS_LOG_TEMPLATE_DEFINE ("TopologyBuilder")
+{
+}
+
+template <class SwitchType>
+void
+TopologyBuilder<SwitchType>::CreateUniqueNode (id_t nodeId, NodeType nodeType)
+{
+  if (nodeType == NodeType::Switch)
+    {
+      if (m_switches.find (nodeId) == m_switches.end ())
+        {
+          NS_LOG_INFO ("Creating the Switch " << nodeId);
+          auto ret = m_switches.emplace (nodeId, SwitchType (nodeId));
+          NS_ABORT_MSG_IF (ret.second == false,
+                           "Trying to insert a duplicate node with id " << nodeId);
+        }
+    }
+  else if (nodeType == NodeType::Terminal)
+    {
+      if (m_terminals.find (nodeId) == m_terminals.end ())
+        {
+          NS_LOG_INFO ("Creating the Terminal " << nodeId);
+          auto ret = m_terminals.emplace (nodeId, CreateObject<Node> ());
+          NS_ABORT_MSG_IF (ret.second == false,
+                           "Trying to insert a duplicate node with id " << nodeId);
+        }
+    }
+  else
+    NS_ABORT_MSG ("Node Id " << nodeId << " has an invalid node type");
+}
+
+template <class SwitchType>
+void
+TopologyBuilder<SwitchType>::CreateNodes (tinyxml2::XMLNode *rootNode)
+{
+  auto netTopElement = rootNode->FirstChildElement ("NetworkTopology");
+  NS_ABORT_MSG_IF (netTopElement == nullptr, "NetworkTopology Element not found");
+
+  auto linkElement = netTopElement->FirstChildElement ("Link");
+
+  while (linkElement != nullptr)
+    {
+      auto linkDetElement = linkElement->FirstChildElement ("LinkElement");
+
+      while (linkDetElement != nullptr)
+        {
+          // Create source node if it does not exist
+          id_t src_node_id;
+          linkDetElement->QueryAttribute ("SourceNode", &src_node_id);
+          auto src_node_type =
+              static_cast<NodeType> (*linkDetElement->Attribute ("SourceNodeType"));
+          CreateUniqueNode (src_node_id, src_node_type);
+
+          // Create destination node if it does not exist
+          id_t dst_node_id;
+          linkDetElement->QueryAttribute ("DestinationNode", &dst_node_id);
+          auto dst_node_type =
+              static_cast<NodeType> (*linkDetElement->Attribute ("DestinationNodeType"));
+          CreateUniqueNode (dst_node_id, dst_node_type);
+
+          linkDetElement = linkDetElement->NextSiblingElement ("LinkElement");
+        }
+      linkElement = linkElement->NextSiblingElement ("Link");
+    }
+}
+
+// FIXME This needs to be removed at the end of the refactoring stage
 #include "topology-builder.cc"
 
 #endif /* TOPOLOGY_BUILDER_H */
