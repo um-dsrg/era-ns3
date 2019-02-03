3793a742096a312cfebd02c1092f905ca96acffb
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-03 14:05:07 +0100

    Comment code in the OSPF switch to enable successful compilation

--- examples/ospf-network/ospf-switch.cc
+++ examples/ospf-network/ospf-switch.cc
@@ -16,59 +16,59 @@ OspfSwitch::OspfSwitch (id_t id) : SwitchBase (id)
 void
 OspfSwitch::SetPacketHandlingMechanism ()
 {
-  for (auto &linkNetDevice : m_linkNetDeviceTable)
-    {
-      linkNetDevice.second->TraceConnect ("PhyTxBegin", std::to_string (linkNetDevice.first),
-                                          MakeCallback (&OspfSwitch::LogPacketTransmission, this));
-    }
+  // for (auto &linkNetDevice : m_linkNetDeviceTable)
+  //   {
+  //     linkNetDevice.second->TraceConnect ("PhyTxBegin", std::to_string (linkNetDevice.first),
+  //                                         MakeCallback (&OspfSwitch::LogPacketTransmission, this));
+  //   }
 }
 
-void
-OspfSwitch::InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
-                                       char protocol, FlowId_t flowId)
-{
-  Flow flow (flowId, srcIpAddr, dstIpAddr, portNumber, protocol);
+// void
+// OspfSwitch::InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
+//                                        char protocol, FlowId_t flowId)
+// {
+// Flow flow (flowId, srcIpAddr, dstIpAddr, portNumber, protocol);
 
-  NS_LOG_INFO ("---------------------------------------------------------------");
-  NS_LOG_INFO ("Inserting entry in Switch " << m_id << " routing table.\n" << flow);
+// NS_LOG_INFO ("---------------------------------------------------------------");
+// NS_LOG_INFO ("Inserting entry in Switch " << m_id << " routing table.\n" << flow);
 
-  auto ret = m_routingTable.find (flow);
+// auto ret = m_routingTable.find (flow);
 
-  NS_ABORT_MSG_IF (ret != m_routingTable.end (), "Trying to insert duplicate entry in OSPF switch");
+// NS_ABORT_MSG_IF (ret != m_routingTable.end (), "Trying to insert duplicate entry in OSPF switch");
 
-  m_routingTable.insert ({flow, flowId});
-}
+// m_routingTable.insert ({flow, flowId});
+// }
 
-void
-OspfSwitch::LogPacketTransmission (std::string context, ns3::Ptr<const ns3::Packet> packet)
-{
-  NS_LOG_INFO ("  Switch " << m_id << ": Received a packet (" << packet->GetSize () << "bytes) at "
-                           << Simulator::Now ().GetSeconds () << "s");
+// void
+// OspfSwitch::LogPacketTransmission (std::string context, ns3::Ptr<const ns3::Packet> packet)
+// {
+// NS_LOG_INFO ("  Switch " << m_id << ": Received a packet (" << packet->GetSize () << "bytes) at "
+//                          << Simulator::Now ().GetSeconds () << "s");
 
-  LinkId_t linkId (std::stoul (context));
-  auto linkNetDevice = m_linkNetDeviceTable.find (linkId);
-  NS_ABORT_MSG_IF (linkNetDevice == m_linkNetDeviceTable.end (),
-                   "The net device with link id " << linkId << " was not found");
+// LinkId_t linkId (std::stoul (context));
+// auto linkNetDevice = m_linkNetDeviceTable.find (linkId);
+// NS_ABORT_MSG_IF (linkNetDevice == m_linkNetDeviceTable.end (),
+//                  "The net device with link id " << linkId << " was not found");
 
-  // Remove the packet's Point To Point (PPP) header before passing it to the parse packet function.
-  Ptr<Packet> recvPacket = packet->Copy (); // Copy the packet for parsing purposes
-  PppHeader pppHeader;
+// // Remove the packet's Point To Point (PPP) header before passing it to the parse packet function.
+// Ptr<Packet> recvPacket = packet->Copy (); // Copy the packet for parsing purposes
+// PppHeader pppHeader;
 
-  NS_ABORT_MSG_IF ((recvPacket->PeekHeader (pppHeader) == 0), "The PPP Header was not found");
-  recvPacket->RemoveHeader (pppHeader);
+// NS_ABORT_MSG_IF ((recvPacket->PeekHeader (pppHeader) == 0), "The PPP Header was not found");
+// recvPacket->RemoveHeader (pppHeader);
 
-  Flow flow (ParsePacket (recvPacket, Ipv4L3Protocol::PROT_NUMBER,
-                          true /*ICMP packets are supported in this simulation*/));
+// Flow flow (ParsePacket (recvPacket, Ipv4L3Protocol::PROT_NUMBER,
+//                         true /*ICMP packets are supported in this simulation*/));
 
-  // Log only UDP, and TCP and their ACK packets
-  if (flow.GetProtocol () == Flow::Udp || flow.GetProtocol () == Flow::Tcp ||
-      flow.GetProtocol () == Flow::Ack)
-    {
-      auto tableMatch = m_routingTable.find (flow);
-      NS_ABORT_MSG_IF (tableMatch == m_routingTable.end (), "Routing Table Miss for flow\n"
-                                                                << flow);
+// // Log only UDP, and TCP and their ACK packets
+// if (flow.GetProtocol () == Flow::Udp || flow.GetProtocol () == Flow::Tcp ||
+//     flow.GetProtocol () == Flow::Ack)
+//   {
+//     auto tableMatch = m_routingTable.find (flow);
+//     NS_ABORT_MSG_IF (tableMatch == m_routingTable.end (), "Routing Table Miss for flow\n"
+//                                                               << flow);
 
-      LogQueueEntries (linkNetDevice->second);
-      LogLinkStatistics (linkNetDevice->second, tableMatch->second, packet->GetSize ());
-    }
-}
+//     LogQueueEntries (linkNetDevice->second);
+//     LogLinkStatistics (linkNetDevice->second, tableMatch->second, packet->GetSize ());
+//   }
+// }
--- examples/ospf-network/ospf-switch.h
+++ examples/ospf-network/ospf-switch.h
@@ -13,12 +13,12 @@ public:
   OspfSwitch (id_t id);
 
   void SetPacketHandlingMechanism ();
-  void InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
-                                  char protocol, FlowId_t flowId);
+  // void InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
+  //                                 char protocol, FlowId_t flowId);
 
 private:
-  void LogPacketTransmission (std::string context, ns3::Ptr<const ns3::Packet> packet);
-  std::map<Flow, FlowId_t> m_routingTable;
+  // void LogPacketTransmission (std::string context, ns3::Ptr<const ns3::Packet> packet);
+  // std::map<Flow, FlowId_t> m_routingTable;
 };
 
 #endif /* OSPF_SWITCH_H */
