b52224d3585b13b3cad0beb00ae8a6c27da8850f
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-03 14:04:40 +0100

    Refactor the PPFS Switch class
    The PPFS Switch has a new struct RtFlow that represents the routing table flow entry. The AddEntryToRoutingTable function has been updated to insert elements in this map.

--- examples/ppfs/ppfs-switch.cc
+++ examples/ppfs/ppfs-switch.cc
@@ -23,155 +23,175 @@ PpfsSwitch::PpfsSwitch (id_t id) : SwitchBase (id)
 {
 }
 
-const uint32_t
-PpfsSwitch::GetSeed () const
-{
-  return m_seed;
-}
-
-const uint32_t
-PpfsSwitch::GetRun () const
-{
-  return m_run;
-}
-
 void
-PpfsSwitch::InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
-                                       char protocol, FlowId_t flowId, LinkId_t linkId,
-                                       double flowRatio)
+PpfsSwitch::AddEntryToRoutingTable (uint32_t srcIp, uint32_t dstIp, portNum_t srcPort,
+                                    portNum_t dstPort, FlowProtocol protocol,
+                                    Ptr<NetDevice> forwardingPort)
 {
-  Flow currentFlow (flowId, srcIpAddr, dstIpAddr, portNumber, protocol);
+  // FIXME May make this log more informative.
+  NS_LOG_INFO ("Adding routing table entry in PPFS Switch " << m_id);
 
-  auto ret = m_linkNetDeviceTable.find (linkId);
-  NS_ABORT_MSG_IF (ret == m_linkNetDeviceTable.end (),
-                   "Link " << linkId << " was not found in "
-                           << "the routing table of Switch with Id " << m_id);
-  Ptr<NetDevice> forwardDevice = ret->second;
-
-  NS_LOG_INFO ("---------------------------------------------------------------");
-  NS_LOG_INFO ("Inserting entry in Switch " << m_id << " routing table.\n" << currentFlow);
-
-  auto routingTableEntry = m_routingTable.find (currentFlow);
-  if (routingTableEntry == m_routingTable.end ()) // Route does not exist in table
-    {
-      std::vector<ForwardingAction> forwardingActions;
-      forwardingActions.push_back (ForwardingAction (forwardDevice, flowRatio));
-      m_routingTable.insert ({currentFlow, forwardingActions});
-
-      NS_LOG_INFO (forwardingActions.back ()); // Logging the entry
-    }
-  else // Route already exists in table
-    {
-      std::vector<ForwardingAction> &forwardActions = routingTableEntry->second;
-      ForwardingAction &lastAction = forwardActions.back ();
-      double currentSplitRatio = lastAction.splitRatio + flowRatio;
-      forwardActions.push_back (ForwardingAction (forwardDevice, currentSplitRatio));
-
-      for (auto &action : forwardActions)
-        NS_LOG_INFO (action);
-    }
+  auto ret =
+      m_routingTable.emplace (RtFlow (srcIp, dstIp, srcPort, dstPort, protocol), forwardingPort);
+  NS_ABORT_MSG_IF (ret.second == false, "Unable to add routing table entry in Switch " << m_id);
 }
 
-void
-PpfsSwitch::SetPacketHandlingMechanism ()
+bool
+PpfsSwitch::RtFlow::operator< (const RtFlow &other) const
 {
-  uint32_t numOfDevices = m_node->GetNDevices ();
-  NS_ASSERT (numOfDevices > 0);
-  for (uint32_t currentDevice = 0; currentDevice < numOfDevices; ++currentDevice)
-    {
-      m_node->RegisterProtocolHandler (MakeCallback (&PpfsSwitch::ReceiveFromDevice, this),
-                                       /*all protocols*/ 0, m_node->GetDevice (currentDevice),
-                                       /*disable promiscuous mode*/ false);
-    }
+  // FIXME Implement this
+  return 1 < 5;
 }
 
-void
-PpfsSwitch::ForwardPacket (ns3::Ptr<const ns3::Packet> packet, uint16_t protocol,
-                           const Address &dst)
-{
-  Flow flow (
-      ParsePacket (packet, protocol, false /*This simulation does not handle ICMP packets*/));
+// const uint32_t
+// PpfsSwitch::GetSeed () const
+// {
+//   return m_seed;
+// }
 
-  auto ret = m_routingTable.find (flow);
-  NS_ABORT_MSG_IF (ret == m_routingTable.end (), "Routing Table Miss\n" << flow);
+// const uint32_t
+// PpfsSwitch::GetRun () const
+// {
+//   return m_run;
+// }
 
-  NS_LOG_INFO ("  Switch " << m_id << ": Forwarding packet at " << Simulator::Now ().GetSeconds ()
-                           << "s");
+// void
+// PpfsSwitch::InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
+//                                        char protocol, FlowId_t flowId, LinkId_t linkId,
+//                                        double flowRatio)
+// {
+//   Flow currentFlow (flowId, srcIpAddr, dstIpAddr, portNumber, protocol);
 
-  Ptr<NetDevice> forwardingPort (GetPort (ret->second));
-  uint32_t packetSizeInclP2pHdr (packet->GetSize () + 2);
+//   auto ret = m_linkNetDeviceTable.find (linkId);
+//   NS_ABORT_MSG_IF (ret == m_linkNetDeviceTable.end (),
+//                    "Link " << linkId << " was not found in "
+//                            << "the routing table of Switch with Id " << m_id);
+//   Ptr<NetDevice> forwardDevice = ret->second;
 
-  // To get the flow id use the one from the routing table *ret*, because the variable *flow* does
-  // not have a valid flow id set.
-  LogLinkStatistics (forwardingPort, ret->first.id, packetSizeInclP2pHdr);
-  bool sendSuccessful = forwardingPort->Send (packet->Copy (), dst, protocol);
-  if (sendSuccessful == false)
-    NS_LOG_INFO ("WARNING: Packet Transmission failed");
+//   NS_LOG_INFO ("---------------------------------------------------------------");
+//   NS_LOG_INFO ("Inserting entry in Switch " << m_id << " routing table.\n" << currentFlow);
 
-  LogQueueEntries (forwardingPort); // Log the net device's queue details
-}
+//   auto routingTableEntry = m_routingTable.find (currentFlow);
+//   if (routingTableEntry == m_routingTable.end ()) // Route does not exist in table
+//     {
+//       std::vector<ForwardingAction> forwardingActions;
+//       forwardingActions.push_back (ForwardingAction (forwardDevice, flowRatio));
+//       m_routingTable.insert ({currentFlow, forwardingActions});
 
-void
-PpfsSwitch::SetRandomNumberGenerator ()
-{
-  /*
-   * The Seed and run are stored before generating the random variable because the run variable will
-   * be automatically incremented once a UniformRandomVariable is created.
-   */
-  m_seed = RandomGeneratorManager::GetSeed ();
-  m_run = RandomGeneratorManager::GetRun ();
+//       NS_LOG_INFO (forwardingActions.back ()); // Logging the entry
+//     }
+//   else // Route already exists in table
+//     {
+//       std::vector<ForwardingAction> &forwardActions = routingTableEntry->second;
+//       ForwardingAction &lastAction = forwardActions.back ();
+//       double currentSplitRatio = lastAction.splitRatio + flowRatio;
+//       forwardActions.push_back (ForwardingAction (forwardDevice, currentSplitRatio));
 
-  m_uniformRandomVariable = RandomGeneratorManager::CreateUniformRandomVariable (0.0, 1.0);
-}
+//       for (auto &action : forwardActions)
+//         NS_LOG_INFO (action);
+//     }
+// }
 
-void
-PpfsSwitch::ReceiveFromDevice (Ptr<NetDevice> incomingPort, Ptr<const Packet> packet,
-                               uint16_t protocol, const Address &src, const Address &dst,
-                               NetDevice::PacketType packetType)
-{
-  NS_LOG_INFO ("  Switch " << m_id << ": Received a packet (" << packet->GetSize () << "bytes) at "
-                           << Simulator::Now ().GetSeconds () << "s");
+// void
+// PpfsSwitch::SetPacketHandlingMechanism ()
+// {
+//   uint32_t numOfDevices = m_node->GetNDevices ();
+//   NS_ASSERT (numOfDevices > 0);
+//   for (uint32_t currentDevice = 0; currentDevice < numOfDevices; ++currentDevice)
+//     {
+//       m_node->RegisterProtocolHandler (MakeCallback (&PpfsSwitch::ReceiveFromDevice, this),
+//                                        /*all protocols*/ 0, m_node->GetDevice (currentDevice),
+//                                        /*disable promiscuous mode*/ false);
+//     }
+// }
 
-  ForwardPacket (packet, protocol, dst); // Forward the packet
-}
+// void
+// PpfsSwitch::ForwardPacket (ns3::Ptr<const ns3::Packet> packet, uint16_t protocol,
+//                            const Address &dst)
+// {
+//   Flow flow (
+//       ParsePacket (packet, protocol, false /*This simulation does not handle ICMP packets*/));
 
-ns3::Ptr<ns3::NetDevice>
-PpfsSwitch::GetPort (const std::vector<PpfsSwitch::ForwardingAction> &forwardActions)
-{
-  double randomNumber (GenerateRandomNumber ()); // Generate random number
-  double lhs = 0.0;
-  double rhs = 0.0;
+//   auto ret = m_routingTable.find (flow);
+//   NS_ABORT_MSG_IF (ret == m_routingTable.end (), "Routing Table Miss\n" << flow);
 
-  typedef std::vector<PpfsSwitch::ForwardingAction>::size_type sizeType;
+//   NS_LOG_INFO ("  Switch " << m_id << ": Forwarding packet at " << Simulator::Now ().GetSeconds ()
+//                            << "s");
 
-  // Loop through the routing table until a match is found
-  for (sizeType index = 0; index < forwardActions.size (); ++index)
-    { // A match is found based on the equation (lhs <= value < rhs)
-      lhs = (index == 0) ? 0.0 : forwardActions[index - 1].splitRatio;
-      rhs = forwardActions[index].splitRatio;
+//   Ptr<NetDevice> forwardingPort (GetPort (ret->second));
+//   uint32_t packetSizeInclP2pHdr (packet->GetSize () + 2);
 
-      if ((lhs <= randomNumber) && (randomNumber < rhs))
-        return forwardActions[index].port; // Match found
+//   // To get the flow id use the one from the routing table *ret*, because the variable *flow* does
+//   // not have a valid flow id set.
+//   LogLinkStatistics (forwardingPort, ret->first.id, packetSizeInclP2pHdr);
+//   bool sendSuccessful = forwardingPort->Send (packet->Copy (), dst, protocol);
+//   if (sendSuccessful == false)
+//     NS_LOG_INFO ("WARNING: Packet Transmission failed");
 
-      /*
-       * If we are at the end and of the forwarding table and no match has been found yet the packet
-       * should be forwarded at the last forwarding slot. When such instance occur a log entry will
-       * be output to ease debugging.
-       */
-      if (index == (forwardActions.size () - 1))
-        {
-          NS_LOG_INFO ("The random number " << randomNumber << " did not fit any slot in"
-                                            << " the forwarding table.");
-          return forwardActions[index].port;
-        }
-    }
+//   LogQueueEntries (forwardingPort); // Log the net device's queue details
+// }
 
-  NS_FATAL_ERROR ("Forwarding port number for packet not found.");
-  return 0; // This return statement will never be hit
-}
+// void
+// PpfsSwitch::SetRandomNumberGenerator ()
+// {
+//   /*
+//    * The Seed and run are stored before generating the random variable because the run variable will
+//    * be automatically incremented once a UniformRandomVariable is created.
+//    */
+//   m_seed = RandomGeneratorManager::GetSeed ();
+//   m_run = RandomGeneratorManager::GetRun ();
 
-double
-PpfsSwitch::GenerateRandomNumber ()
-{
-  return m_uniformRandomVariable->GetValue ();
-}
+//   m_uniformRandomVariable = RandomGeneratorManager::CreateUniformRandomVariable (0.0, 1.0);
+// }
+
+// void
+// PpfsSwitch::ReceiveFromDevice (Ptr<NetDevice> incomingPort, Ptr<const Packet> packet,
+//                                uint16_t protocol, const Address &src, const Address &dst,
+//                                NetDevice::PacketType packetType)
+// {
+//   NS_LOG_INFO ("  Switch " << m_id << ": Received a packet (" << packet->GetSize () << "bytes) at "
+//                            << Simulator::Now ().GetSeconds () << "s");
+
+//   ForwardPacket (packet, protocol, dst); // Forward the packet
+// }
+
+// ns3::Ptr<ns3::NetDevice>
+// PpfsSwitch::GetPort (const std::vector<PpfsSwitch::ForwardingAction> &forwardActions)
+// {
+//   double randomNumber (GenerateRandomNumber ()); // Generate random number
+//   double lhs = 0.0;
+//   double rhs = 0.0;
+
+//   typedef std::vector<PpfsSwitch::ForwardingAction>::size_type sizeType;
+
+//   // Loop through the routing table until a match is found
+//   for (sizeType index = 0; index < forwardActions.size (); ++index)
+//     { // A match is found based on the equation (lhs <= value < rhs)
+//       lhs = (index == 0) ? 0.0 : forwardActions[index - 1].splitRatio;
+//       rhs = forwardActions[index].splitRatio;
+
+//       if ((lhs <= randomNumber) && (randomNumber < rhs))
+//         return forwardActions[index].port; // Match found
+
+//       /*
+//        * If we are at the end and of the forwarding table and no match has been found yet the packet
+//        * should be forwarded at the last forwarding slot. When such instance occur a log entry will
+//        * be output to ease debugging.
+//        */
+//       if (index == (forwardActions.size () - 1))
+//         {
+//           NS_LOG_INFO ("The random number " << randomNumber << " did not fit any slot in"
+//                                             << " the forwarding table.");
+//           return forwardActions[index].port;
+//         }
+//     }
+
+//   NS_FATAL_ERROR ("Forwarding port number for packet not found.");
+//   return 0; // This return statement will never be hit
+// }
+
+// double
+// PpfsSwitch::GenerateRandomNumber ()
+// {
+//   return m_uniformRandomVariable->GetValue ();
+// }
--- examples/ppfs/ppfs-switch.h
+++ examples/ppfs/ppfs-switch.h
@@ -17,56 +17,77 @@ public:
   PpfsSwitch () = default;
   PpfsSwitch (id_t id);
 
-  const uint32_t GetSeed () const;
-  const uint32_t GetRun () const;
+  void AddEntryToRoutingTable (uint32_t srcIp, uint32_t dstIp, portNum_t srcPort, portNum_t dstPort,
+                               FlowProtocol protocol, ns3::Ptr<ns3::NetDevice> forwardingPort);
 
-  void InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
-                                  char protocol, FlowId_t flowId, LinkId_t linkId,
-                                  double flowRatio);
-  void SetPacketHandlingMechanism ();
-  void ForwardPacket (ns3::Ptr<const ns3::Packet> packet, uint16_t protocol,
-                      const ns3::Address &dst);
-  void SetRandomNumberGenerator ();
+  // const uint32_t GetSeed () const;
+  // const uint32_t GetRun () const;
+
+  // void InsertEntryInRoutingTable (uint32_t srcIpAddr, uint32_t dstIpAddr, uint16_t portNumber,
+  //                                 char protocol, FlowId_t flowId, LinkId_t linkId,
+  //                                 double flowRatio);
+  // void SetPacketHandlingMechanism ();
+  // void ForwardPacket (ns3::Ptr<const ns3::Packet> packet, uint16_t protocol,
+  //                     const ns3::Address &dst);
+  // void SetRandomNumberGenerator ();
 
 private:
-  void ReceiveFromDevice (ns3::Ptr<ns3::NetDevice> incomingPort, ns3::Ptr<const ns3::Packet> packet,
-                          uint16_t protocol, ns3::Address const &src, ns3::Address const &dst,
-                          ns3::NetDevice::PacketType packetType);
-
-  struct ForwardingAction
+  struct RtFlow
   {
-    ns3::Ptr<ns3::NetDevice> port;
-    double splitRatio;
-
-    ForwardingAction (ns3::Ptr<ns3::NetDevice> port, double splitRatio)
-        : port (port), splitRatio (splitRatio)
+    RtFlow (uint32_t srcIp, uint32_t dstIp, portNum_t srcPort, portNum_t dstPort,
+            FlowProtocol protocol)
+        : srcIp{srcIp}, dstIp{dstIp}, srcPort{srcPort}, dstPort{dstPort}, protocol{protocol}
     {
     }
 
-    friend std::ostream &
-    operator<< (std::ostream &output, ForwardingAction &value)
-    {
-      output << "Split Ratio " << value.splitRatio << "\n";
-      output << "----------------------";
-      return output;
-    }
+    bool operator< (const RtFlow &other) const;
+
+    uint32_t srcIp{0};
+    uint32_t dstIp{0};
+    portNum_t srcPort{0};
+    portNum_t dstPort{0};
+    FlowProtocol protocol;
   };
 
-  ns3::Ptr<ns3::NetDevice> GetPort (const std::vector<ForwardingAction> &forwardActions);
-  double GenerateRandomNumber ();
+  std::map<RtFlow, ns3::Ptr<ns3::NetDevice>> m_routingTable;
 
-  /*
-   * Routing Table
-   *
-   * Key -> Flow, Value -> Vector of Forwarding actions.
-   * This map stores the routing table which is used to map a flow to
-   * a set of forwarding actions.
-   */
-  std::map<Flow, std::vector<ForwardingAction>> m_routingTable;
-  ns3::Ptr<ns3::UniformRandomVariable> m_uniformRandomVariable; //!< Used for flow splitting
+  // void ReceiveFromDevice (ns3::Ptr<ns3::NetDevice> incomingPort, ns3::Ptr<const ns3::Packet> packet,
+  //                         uint16_t protocol, ns3::Address const &src, ns3::Address const &dst,
+  //                         ns3::NetDevice::PacketType packetType);
 
-  uint32_t m_seed;
-  uint32_t m_run;
+  // struct ForwardingAction
+  // {
+  //   ns3::Ptr<ns3::NetDevice> port;
+  //   double splitRatio;
+
+  //   ForwardingAction (ns3::Ptr<ns3::NetDevice> port, double splitRatio)
+  //       : port (port), splitRatio (splitRatio)
+  //   {
+  //   }
+
+  //   friend std::ostream &
+  //   operator<< (std::ostream &output, ForwardingAction &value)
+  //   {
+  //     output << "Split Ratio " << value.splitRatio << "\n";
+  //     output << "----------------------";
+  //     return output;
+  //   }
+  // };
+
+  // ns3::Ptr<ns3::NetDevice> GetPort (const std::vector<ForwardingAction> &forwardActions);
+  // double GenerateRandomNumber ();
+
+  //  * Routing Table
+  //  *
+  //  * Key -> Flow, Value -> Vector of Forwarding actions.
+  //  * This map stores the routing table which is used to map a flow to
+  //  * a set of forwarding actions.
+
+  // std::map<Flow, std::vector<ForwardingAction>> m_routingTable;
+  // ns3::Ptr<ns3::UniformRandomVariable> m_uniformRandomVariable; //!< Used for flow splitting
+
+  // uint32_t m_seed;
+  // uint32_t m_run;
 };
 
 #endif /* PPFS_SWITCH_H */
