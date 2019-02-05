f87fd42fcecfc0090a125b49190ace7a1e98245b
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-05 10:19:41 +0100

    Update the ReceiverApp to handle packet reception
    The ReceiverApp now handles packet reception.

--- common-code/receiver-app.cc
+++ common-code/receiver-app.cc
@@ -1,6 +1,7 @@
 #include "ns3/log.h"
 #include "ns3/tcp-socket-factory.h"
 #include "ns3/udp-socket-factory.h"
+#include "ns3/inet-socket-address.h"
 
 #include "receiver-app.h"
 
@@ -8,8 +9,28 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("ReceiverApp");
 
+// TODO: Set this in the app base class and remove the node parameter
+Ptr<Socket> CreateSocket(Ptr<Node> node, FlowProtocol protocol)
+{
+  if (protocol == FlowProtocol::Tcp) { // Tcp Socket
+    return Socket::CreateSocket(node, TcpSocketFactory::GetTypeId ());
+  } else if (protocol == FlowProtocol::Udp) { // Udp Socket
+    return Socket::CreateSocket(node, UdpSocketFactory::GetTypeId ());
+  } else {
+    NS_ABORT_MSG("Cannot create non TCP/UDP socket");
+  }
+}
+
 ReceiverApp::ReceiverApp(const Flow& flow)
 {
+  for (const auto& path : flow.GetPaths()) {
+    PathInformation pathInfo;
+    pathInfo.dstPort = path.dstPort;
+    pathInfo.rxSocket = CreateSocket(flow.dstNode->GetNode(), flow.protocol);
+    pathInfo.dstAddress = Address(InetSocketAddress(flow.dstNode->GetIpAddress(), path.dstPort));
+
+    m_pathInfoContainer.push_back(pathInfo);
+  }
 }
 
 ReceiverApp::~ReceiverApp()
@@ -19,10 +40,27 @@ ReceiverApp::~ReceiverApp()
 
 void ReceiverApp::StartApplication()
 {
-  NS_LOG_UNCOND("Receiver started");
+  m_appRunning = true;
+  NS_LOG_UNCOND("Receiver application started");
+
+  // Initialise socket connections
+  for (const auto& pathInfo : m_pathInfoContainer) {
+    if (pathInfo.rxSocket->Bind(pathInfo.dstAddress) == -1) {
+      NS_ABORT_MSG("Failed to bind socket");
+    }
+
+    pathInfo.rxSocket->Listen();
+    pathInfo.rxSocket->ShutdownSend (); // Half close the connection;
+    pathInfo.rxSocket->SetRecvCallback (MakeCallback(&ReceiverApp::PacketReceived, this));
+  }
 }
 
 void ReceiverApp::StopApplication()
 {
   NS_LOG_UNCOND("Receiver stopped");
+}
+
+void ReceiverApp::PacketReceived(Ptr<Socket> socket)
+{
+  NS_LOG_UNCOND("A packet has been received");
 }
--- common-code/receiver-app.h
+++ common-code/receiver-app.h
@@ -1,6 +1,8 @@
 #ifndef receiver_app_h
 #define receiver_app_h
 
+#include "ns3/socket.h"
+#include "ns3/ipv4-address.h"
 #include "ns3/applications-module.h"
 
 #include "flow.h"
@@ -14,6 +16,18 @@ public:
 private:
   virtual void StartApplication ();
   virtual void StopApplication ();
+  void PacketReceived (ns3::Ptr<ns3::Socket> socket);
+
+  struct PathInformation
+  {
+    portNum_t dstPort;
+    ns3::Ptr<ns3::Socket> rxSocket; //!< The socket to transmit data on the given path.
+    ns3::Address dstAddress; //!< The path's destination address.
+  };
+
+  bool m_appRunning {false}; //!< Flag that determines the application's running stage
+  dataRate_t m_dataRate {0.0}; //!< Application's data rate
+  std::vector<PathInformation> m_pathInfoContainer;
 };
 
 #endif /* receiver_app_h */
