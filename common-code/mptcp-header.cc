10082a4dbaf55a25265310898cc715fe14001f34
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-11 13:02:28 +0100

    Update style to MptcpHeader class

--- common-code/mptcp-header.cc
+++ common-code/mptcp-header.cc
@@ -2,46 +2,38 @@
 
 using namespace ns3;
 
-TypeId MptcpHeader::GetTypeId()
-{
-  static TypeId tid = TypeId("MptcpHeader").SetParent<Header>().AddConstructor<MptcpHeader>();
-  return tid;
+TypeId MptcpHeader::GetTypeId() {
+    static TypeId tid = TypeId("MptcpHeader").SetParent<Header>().AddConstructor<MptcpHeader>();
+    return tid;
 }
 
-TypeId MptcpHeader::GetInstanceTypeId() const
-{
-  return GetTypeId();
+TypeId MptcpHeader::GetInstanceTypeId() const {
+    return GetTypeId();
 }
 
-uint32_t MptcpHeader::GetSerializedSize() const
-{
-  // This header contains a 64-bit unsigned integer that represents
-  // the packet number.
-  return 8; // Size of the header in bytes.
+uint32_t MptcpHeader::GetSerializedSize() const {
+    // This header contains a 64-bit unsigned integer that represents
+    // the packet number.
+    return 8; // Size of the header in bytes.
 }
 
-void MptcpHeader::Serialize(Buffer::Iterator start) const
-{
-  start.WriteHtonU64(m_packetNumber);
+void MptcpHeader::Serialize(Buffer::Iterator start) const {
+    start.WriteHtonU64(m_packetNumber);
 }
 
-uint32_t MptcpHeader::Deserialize(Buffer::Iterator start)
-{
-  m_packetNumber = start.ReadNtohU64();
-  return 8; // The number of bytes consumed.
+uint32_t MptcpHeader::Deserialize(Buffer::Iterator start) {
+    m_packetNumber = start.ReadNtohU64();
+    return 8; // The number of bytes consumed.
 }
 
-void MptcpHeader::Print(std::ostream &os) const
-{
-  os << "Packet number " << m_packetNumber;
+void MptcpHeader::Print(std::ostream &os) const {
+    os << "Packet number " << m_packetNumber;
 }
 
-void MptcpHeader::SetPacketNumber(packetNumber_t packetNumber)
-{
-  m_packetNumber = packetNumber;
+void MptcpHeader::SetPacketNumber(packetNumber_t packetNumber) {
+    m_packetNumber = packetNumber;
 }
 
-packetNumber_t MptcpHeader::GetPacketNumber() const
-{
-  return m_packetNumber;
+packetNumber_t MptcpHeader::GetPacketNumber() const {
+    return m_packetNumber;
 }
