6095e38d59a7d1d802806c7d647ae5dbb767e32c
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-11 16:53:59 +0100

    Remove the unused id typedefs

--- common-code/definitions.h
+++ common-code/definitions.h
@@ -10,10 +10,6 @@ typedef double delay_t;
 typedef double dataRate_t;
 typedef uint32_t packetSize_t;
 typedef uint64_t packetNumber_t;
-// TODO Remove the below typedefs
-//typedef uint32_t LinkId_t;
-//typedef uint32_t NodeId_t;
-//typedef uint32_t FlowId_t;
 
 // Enum definitions /////////////////////////////////////////////////////////
 enum class NodeType : char { Switch = 'S', Terminal = 'T' };
