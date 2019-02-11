95aa577155f378307d3a09db50791a45222ebbf6
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-11 13:05:06 +0100

    Move the CreateSocket function in the TransmitterApp class + Abort transmission if the MaximumSegmentSize limit is exceeded

--- common-code/transmitter-app.cc
+++ common-code/transmitter-app.cc
@@ -1,7 +1,9 @@
 #include <math.h>
 
 #include "ns3/log.h"
+#include "ns3/uinteger.h"
 #include "ns3/simulator.h"
+#include "ns3/tcp-socket.h"
 #include "ns3/tcp-socket-factory.h"
 #include "ns3/udp-socket-factory.h"
 
@@ -13,130 +15,134 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("TransmitterApp");
 
-Ptr<Socket> createSocket(Ptr<Node> srcNode, FlowProtocol protocol)
-{
-  if (protocol == FlowProtocol::Tcp) { // Tcp Socket
-    return Socket::CreateSocket(srcNode, TcpSocketFactory::GetTypeId ());
-  } else if (protocol == FlowProtocol::Udp) { // Udp Socket
-    return Socket::CreateSocket(srcNode, UdpSocketFactory::GetTypeId ());
-  } else {
-    NS_ABORT_MSG("Cannot create non TCP/UDP socket");
-  }
+TransmitterApp::TransmitterApp(const Flow& flow) : m_dataRate(flow.dataRate), m_packetSize(flow.packetSize) {
+    for (const auto& path : flow.GetDataPaths()) {
+        PathInformation pathInfo;
+        pathInfo.srcPort = path.srcPort;
+        pathInfo.txSocket = CreateSocket(flow.srcNode->GetNode(), flow.protocol);
+        pathInfo.dstAddress = Address(InetSocketAddress(flow.dstNode->GetIpAddress(), path.dstPort));
+
+        auto ret =  m_pathInfoContainer.emplace(path.id, pathInfo);
+        NS_ABORT_MSG_IF(ret.second == false, "Trying to insert duplicate path: " << path.id <<
+                                             " for flow " << flow.id);
+
+        // Calculate the split ratio at path level
+        m_pathSplitRatio.push_back(std::make_pair((path.dataRate.GetBitRate() / flow.dataRate.GetBitRate()),
+                                                  path.id));
+
+        if (flow.protocol == FlowProtocol::Tcp) {
+            auto tcpSocket = ns3::DynamicCast<ns3::TcpSocket>(pathInfo.txSocket);
+            ns3::UintegerValue segmentSize;
+            tcpSocket->GetAttribute("SegmentSize", segmentSize);
+            auto pktSizeInclHdrs {flow.packetSize + MptcpHeader().GetSerializedSize()};
+
+            if (pktSizeInclHdrs > segmentSize.Get()) {
+                NS_ABORT_MSG("The packet size for Flow " << flow.id << " is larger than the MSS.\n"
+                             "Flow packet size (excl. Header): " << flow.packetSize << "\n" <<
+                             "Flow packet size (incl. Header): " << pktSizeInclHdrs << "\n" <<
+                             "Maximum Segment Size: " << segmentSize.Get());
+            }
+        }
+    }
+
+    // Sort the split ratio vector in ascending order
+    std::sort(m_pathSplitRatio.begin(), m_pathSplitRatio.end(), std::greater<>());
+
+    // Calculate the cumulative split ratio
+    for (auto index = 1; index < m_pathSplitRatio.size(); ++index) {
+        m_pathSplitRatio[index].first += m_pathSplitRatio[index - 1].first;
+    }
+
+    // Setup the random number generator
+    m_uniformRandomVariable = RandomGeneratorManager::CreateUniformRandomVariable(0.0, 1.0);
+
+    // If the number is very close to 1, set it equal to 1
+    if (Abs(m_pathSplitRatio.back().first - 1.0) <= 1e-5) {
+        m_pathSplitRatio.back().first = 1.0;
+    }
+
+    NS_ABORT_MSG_IF(m_pathSplitRatio.back().first != 1.0,
+                    "The final split ratio is not equal to 1. Split Ratio: " <<
+                    m_pathSplitRatio.back().first);
+
+    // Calculate the transmission interval
+    double pktSizeBits = static_cast<double>(m_packetSize * 8);
+    double transmissionInterval = pktSizeBits / static_cast<double>(m_dataRate.GetBitRate());
+    NS_ABORT_MSG_IF(transmissionInterval <= 0 || isnan(transmissionInterval),
+                    "The transmission interval cannot be less than or equal to 0 OR nan. "
+                    "Transmission interval: " << transmissionInterval);
+    m_transmissionInterval = Seconds(transmissionInterval);
 }
 
-TransmitterApp::TransmitterApp(const Flow& flow) : m_dataRate(flow.dataRate),
-                                                   m_packetSize(flow.packetSize)
-{
-  for (const auto& path : flow.GetDataPaths()) {
-    // Store the path information
-    PathInformation pathInfo;
-    pathInfo.srcPort = path.srcPort;
-    pathInfo.txSocket = createSocket(flow.srcNode->GetNode(), flow.protocol);
-    pathInfo.dstAddress = Address(InetSocketAddress(flow.dstNode->GetIpAddress(), path.dstPort));
-
-    auto ret =  m_pathInfoContainer.emplace(path.id, pathInfo);
-    NS_ABORT_MSG_IF(ret.second == false, "Trying to insert duplicate path: " << path.id <<
-                    " for flow " << flow.id);
-
-    // Calculate the split ratio at path level
-    m_pathSplitRatio.push_back(std::make_pair((path.dataRate.GetBitRate() /
-                                               flow.dataRate.GetBitRate()),
-                                              path.id));
-  }
-
-  // Sort the split ratio vector in ascending order
-  std::sort(m_pathSplitRatio.begin(), m_pathSplitRatio.end(), std::greater<>());
-
-  // Calculate the cumulative split ratio
-  for (auto index = 1; index < m_pathSplitRatio.size(); ++index) {
-    m_pathSplitRatio[index].first += m_pathSplitRatio[index - 1].first;
-  }
-
-  // Setup the random number generator
-  m_uniformRandomVariable = RandomGeneratorManager::CreateUniformRandomVariable(0.0, 1.0);
-
-  // If the number is very close to 1, set it equal to 1
-  if (Abs(m_pathSplitRatio.back().first - 1.0) <= 1e-5) {
-    m_pathSplitRatio.back().first = 1.0;
-  }
-
-  NS_ABORT_MSG_IF(m_pathSplitRatio.back().first != 1.0,
-                  "The final split ratio is not equal to 1. Split Ratio: " <<
-                  m_pathSplitRatio.back().first);
-
-  // Calculate the transmission interval
-  double pktSizeBits = static_cast<double>(m_packetSize * 8);
-  double transmissionInterval = pktSizeBits / static_cast<double>(m_dataRate.GetBitRate());
-  NS_ABORT_MSG_IF(transmissionInterval <= 0 || isnan(transmissionInterval),
-                  "The transmission interval cannot be less than or equal to 0 OR nan. "
-                  "Transmission interval: " << transmissionInterval);
-  m_transmissionInterval = Seconds(transmissionInterval);
+Ptr<Socket> TransmitterApp::CreateSocket(Ptr<Node> srcNode, FlowProtocol protocol) {
+    if (protocol == FlowProtocol::Tcp) { // Tcp Socket
+        return Socket::CreateSocket(srcNode, TcpSocketFactory::GetTypeId ());
+    } else if (protocol == FlowProtocol::Udp) { // Udp Socket
+        return Socket::CreateSocket(srcNode, UdpSocketFactory::GetTypeId ());
+    } else {
+        NS_ABORT_MSG("Cannot create non TCP/UDP socket");
+    }
 }
 
-TransmitterApp::~TransmitterApp()
-{
+TransmitterApp::~TransmitterApp() {
   // TODO: Set sockets to 0 to close them.
 }
 
-void TransmitterApp::StartApplication()
-{
-  m_appRunning = true;
+void TransmitterApp::StartApplication() {
+    m_appRunning = true;
 
-  NS_LOG_UNCOND("Transmitter application started");
+    NS_LOG_UNCOND("Transmitter application started");
 
-  // Initialise socket connections
-  for (const auto& pathPair : m_pathInfoContainer) {
-    const auto& pathInfo = pathPair.second;
+    // Initialise socket connections
+    for (const auto& pathPair : m_pathInfoContainer) {
+        const auto& pathInfo = pathPair.second;
 
-    // Create source socket
-    InetSocketAddress srcAddr = InetSocketAddress(Ipv4Address::GetAny(), pathInfo.srcPort);
+        // Create source socket
+        InetSocketAddress srcAddr = InetSocketAddress(Ipv4Address::GetAny(), pathInfo.srcPort);
 
-    if (pathInfo.txSocket->Bind(srcAddr) == -1) {
-      NS_ABORT_MSG("Failed to bind socket");
+        if (pathInfo.txSocket->Bind(srcAddr) == -1) {
+            NS_ABORT_MSG("Failed to bind socket");
+        }
+
+        if (pathInfo.txSocket->Connect(pathInfo.dstAddress) == -1) {
+            NS_ABORT_MSG("Failed to connect socket");
+        }
     }
 
-    if (pathInfo.txSocket->Connect(pathInfo.dstAddress) == -1) {
-      NS_ABORT_MSG("Failed to connect socket");
+    TransmitPacket();
+}
+
+void TransmitterApp::StopApplication() {
+    NS_LOG_UNCOND("Application stopped");
+
+    m_appRunning = false;
+    Simulator::Cancel (m_sendEvent);
+}
+
+void TransmitterApp::TransmitPacket() {
+    if (m_appRunning) {
+        auto randNum = GetRandomNumber();
+        auto transmitPathId = id_t{0};
+
+        for (const auto& pathSplitPair : m_pathSplitRatio) {
+            const auto& splitRatio {pathSplitPair.first};
+            if (randNum <= splitRatio) {
+                transmitPathId = pathSplitPair.second;
+                break;
+            }
+        }
+
+        auto& pathInfo = m_pathInfoContainer.at(transmitPathId);
+
+        MptcpHeader mptcpHeader;
+        mptcpHeader.SetPacketNumber(m_packetNumber++);
+        Ptr<Packet> packet = Create<Packet>(m_packetSize);
+        packet->AddHeader(mptcpHeader);
+        pathInfo.txSocket->Send(packet);
+        m_sendEvent = Simulator::Schedule(m_transmissionInterval, &TransmitterApp::TransmitPacket, this);
     }
-  }
-
-  TransmitPacket();
 }
 
-void TransmitterApp::StopApplication()
-{
-  NS_LOG_UNCOND("Application stopped");
-
-  m_appRunning = false;
-  Simulator::Cancel (m_sendEvent);
-}
-
-void TransmitterApp::TransmitPacket()
-{
-  if (m_appRunning) {
-    auto randNum = GetRandomNumber();
-    auto transmitPathId = id_t{0};
-
-    for (const auto& pathSplitPair : m_pathSplitRatio) {
-      const auto& splitRatio {pathSplitPair.first};
-      if (randNum <= splitRatio) {
-        transmitPathId = pathSplitPair.second;
-        break;
-      }
-    }
-
-    auto& pathInfo = m_pathInfoContainer.at(transmitPathId);
-
-    MptcpHeader mptcpHeader;
-    mptcpHeader.SetPacketNumber(m_packetNumber++);
-    Ptr<Packet> packet = Create<Packet>(m_packetSize);
-    packet->AddHeader(mptcpHeader);
-    pathInfo.txSocket->Send(packet);
-    m_sendEvent = Simulator::Schedule(m_transmissionInterval, &TransmitterApp::TransmitPacket, this);
-  }
-}
-
-inline double TransmitterApp::GetRandomNumber()
-{
-  return m_uniformRandomVariable->GetValue();
+inline double TransmitterApp::GetRandomNumber() {
+    return m_uniformRandomVariable->GetValue();
 }
--- common-code/transmitter-app.h
+++ common-code/transmitter-app.h
@@ -17,34 +17,34 @@
 
 class TransmitterApp : public ns3::Application {
 public:
-  TransmitterApp (const Flow& flow);
-  virtual ~TransmitterApp();
+    TransmitterApp (const Flow& flow);
+    virtual ~TransmitterApp();
 
 private:
-  virtual void StartApplication ();
-  virtual void StopApplication ();
-  void TransmitPacket();
+    ns3::Ptr<ns3::Socket> CreateSocket(ns3::Ptr<ns3::Node> srcNode, FlowProtocol protocol);
 
-  inline double GetRandomNumber ();
+    virtual void StartApplication ();
+    virtual void StopApplication ();
+    void TransmitPacket();
+    inline double GetRandomNumber ();
 
-  struct PathInformation
-  {
-    portNum_t srcPort;
-    ns3::Ptr<ns3::Socket> txSocket; //!< The socket to transmit data on the given path.
-    ns3::Address dstAddress; //!< The path's destination address.
-  };
+    struct PathInformation {
+        portNum_t srcPort;
+        ns3::Ptr<ns3::Socket> txSocket; //!< The socket to transmit data on the given path.
+        ns3::Address dstAddress; //!< The path's destination address.
+    };
 
-  bool m_appRunning {false}; //!< Flag that determines the application's running stage
+    bool m_appRunning {false}; //!< Flag that determines the application's running stage
 
-  ns3::DataRate m_dataRate; //!< Application's data rate
-  ns3::Time m_transmissionInterval; //!< The time between packet transmissions
-  packetSize_t m_packetSize {0}; //!< The packet size in bytes
-  packetNumber_t m_packetNumber {0}; //!< The number of the packet currently transmitted
+    ns3::DataRate m_dataRate; //!< Application's data rate
+    ns3::Time m_transmissionInterval; //!< The time between packet transmissions
+    packetSize_t m_packetSize {0}; //!< The packet size in bytes
+    packetNumber_t m_packetNumber {0}; //!< The number of the packet currently transmitted
 
-  std::map<id_t, PathInformation> m_pathInfoContainer; //!< Path Id | Path Information
-  std::vector<std::pair<double, id_t>> m_pathSplitRatio; //!< Split Ratio | Path Id
-  ns3::EventId m_sendEvent; //!< The Event Id of the pending TransmitPacket event
-  ns3::Ptr<ns3::UniformRandomVariable> m_uniformRandomVariable; //!< Random variable
+    std::map<id_t, PathInformation> m_pathInfoContainer; //!< Path Id | Path Information
+    std::vector<std::pair<double, id_t>> m_pathSplitRatio; //!< Split Ratio | Path Id
+    ns3::EventId m_sendEvent; //!< The Event Id of the pending TransmitPacket event
+    ns3::Ptr<ns3::UniformRandomVariable> m_uniformRandomVariable; //!< Random variable
 };
 
 #endif /* transmitter_app_h */
