ce047fcae0906122c55bc82ab8906fb9a3d2a9ad
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-11 13:10:27 +0100

    Update the indentation settings of the MptcpHeader

--- common-code/mptcp-header.h
+++ common-code/mptcp-header.h
@@ -7,19 +7,19 @@
 
 class MptcpHeader : public ns3::Header {
 public:
-  // must be implemented to become a valid new header.
-  static ns3::TypeId GetTypeId (void);
-  virtual ns3::TypeId GetInstanceTypeId (void) const;
-  virtual uint32_t GetSerializedSize (void) const;
-  virtual void Serialize (ns3::Buffer::Iterator start) const;
-  virtual uint32_t Deserialize (ns3::Buffer::Iterator start);
-  virtual void Print (std::ostream &os) const;
-
-  void SetPacketNumber (packetNumber_t packetNumber);
-  packetNumber_t GetPacketNumber () const;
-
+    // must be implemented to become a valid new header.
+    static ns3::TypeId GetTypeId (void);
+    virtual ns3::TypeId GetInstanceTypeId (void) const;
+    virtual uint32_t GetSerializedSize (void) const;
+    virtual void Serialize (ns3::Buffer::Iterator start) const;
+    virtual uint32_t Deserialize (ns3::Buffer::Iterator start);
+    virtual void Print (std::ostream &os) const;
+    
+    void SetPacketNumber (packetNumber_t packetNumber);
+    packetNumber_t GetPacketNumber () const;
+    
 private:
-  packetNumber_t m_packetNumber {0};
+    packetNumber_t m_packetNumber {0};
 };
 
 #endif /* mptcp_header_h */
