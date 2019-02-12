08bb2bbba6d5d8e0f8ddb00bedba45e82fdbe445
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-12 15:25:30 +0100

    Remove RtFlow functionality from SdnSwitch class

--- common-code/sdn-switch.h
+++ common-code/sdn-switch.h
@@ -24,27 +24,6 @@ private:
                         uint16_t protocol, const ns3::Address &src, const ns3::Address &dst,
                         ns3::NetDevice::PacketType packetType);
 
-    struct RtFlow {
-        RtFlow () = default;
-
-        RtFlow (uint32_t srcIp, uint32_t dstIp, portNum_t srcPort, portNum_t dstPort,
-                FlowProtocol protocol)
-        : srcIp{srcIp}, dstIp{dstIp}, srcPort{srcPort}, dstPort{dstPort}, protocol{protocol}
-        {
-        }
-
-        bool operator< (const RtFlow &other) const;
-
-        uint32_t srcIp{0};
-        uint32_t dstIp{0};
-        portNum_t srcPort{0};
-        portNum_t dstPort{0};
-        FlowProtocol protocol {FlowProtocol::Undefined};
-    };
-
-    friend std::ostream& operator<<(std::ostream& os, const RtFlow& flow);
-    RtFlow ExtractFlowFromPacket (ns3::Ptr<const ns3::Packet> packet, uint16_t protocol);
-
     std::map<RtFlow, ns3::Ptr<ns3::NetDevice>> m_routingTable;
 };
 
