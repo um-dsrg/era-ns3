221821ebbb000fe7c34e8ddc968225a3f58dbdfa
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-12 11:12:56 +0100

    Add an ApplicationBase class to serve as the base class for all Applications

---
+++ common-code/application-base.cc
@@ -0,0 +1,35 @@
+#include "ns3/log.h"
+#include "ns3/simulator.h"
+#include "ns3/tcp-socket-factory.h"
+#include "ns3/udp-socket-factory.h"
+
+#include "application-base.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("ApplicationBase");
+
+const std::map<packetNumber_t, ns3::Time>& ApplicationBase::GetDelayLog() const {
+    return m_delayLog;
+}
+
+ApplicationBase::ApplicationBase(id_t id) : m_id(id) {
+}
+
+ApplicationBase::~ApplicationBase() {
+}
+
+Ptr<Socket> ApplicationBase::CreateSocket(Ptr<Node> srcNode, FlowProtocol protocol) {
+    if (protocol == FlowProtocol::Tcp) { // Tcp Socket
+        return Socket::CreateSocket(srcNode, TcpSocketFactory::GetTypeId ());
+    } else if (protocol == FlowProtocol::Udp) { // Udp Socket
+        return Socket::CreateSocket(srcNode, UdpSocketFactory::GetTypeId ());
+    } else {
+        NS_ABORT_MSG("Cannot create non TCP/UDP socket");
+    }
+}
+
+void ApplicationBase::LogPacketTime(packetNumber_t packetNumber) {
+    NS_LOG_DEBUG("Logging Packet " << packetNumber << " at " << Simulator::Now());
+    m_delayLog.emplace(packetNumber, Simulator::Now());
+}
---
+++ common-code/application-base.h
@@ -0,0 +1,30 @@
+#ifndef application_base_h
+#define application_base_h
+
+#include <map>
+
+#include "ns3/nstime.h"
+#include "ns3/socket.h"
+#include "ns3/application.h"
+
+#include "flow.h"
+#include "definitions.h"
+
+class ApplicationBase : public ns3::Application {
+public:
+    const std::map<packetNumber_t, ns3::Time>& GetDelayLog() const;
+
+protected:
+    ApplicationBase(id_t id);
+    virtual ~ApplicationBase();
+    virtual void StartApplication() = 0;
+    virtual void StopApplication() = 0;
+
+    ns3::Ptr<ns3::Socket> CreateSocket(ns3::Ptr<ns3::Node> srcNode, FlowProtocol protocol);
+    void LogPacketTime(packetNumber_t packetNumber);
+
+    std::map<packetNumber_t, ns3::Time> m_delayLog;
+    id_t m_id{0}; /**< The id of the flow that this application represents. */
+};
+
+#endif /* application_base_h */
