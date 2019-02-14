1153e03fac8c3b16a244d7130c4b0e252baac7fe
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-14 10:09:39 +0100

    Format the RandomGeneratorManager to match the code style

--- common-code/random-generator-manager.h
+++ common-code/random-generator-manager.h
@@ -1,27 +1,24 @@
-#ifndef RANDOM_GENERATOR_MANAGER_H
-#define RANDOM_GENERATOR_MANAGER_H
+#ifndef random_generator_manager_h
+#define random_generator_manager_h
 
 #include <cstdint>
 
-#include "ns3/random-variable-stream.h"
-#include "ns3/rng-seed-manager.h"
 #include "ns3/double.h"
+#include "ns3/rng-seed-manager.h"
+#include "ns3/random-variable-stream.h"
 
-class RandomGeneratorManager
-{
+class RandomGeneratorManager {
+    static uint32_t m_seed;
+    static uint32_t m_run;
 public:
-  static uint32_t GetSeed ();
-  static uint32_t GetRun ();
-  static void SetSeed (uint32_t seed);
-  static void SetRun (uint32_t run);
-
-  static ns3::Ptr<ns3::UniformRandomVariable> CreateUniformRandomVariable (double min, double max);
-
+    static uint32_t GetSeed ();
+    static uint32_t GetRun ();
+    static void SetSeed (uint32_t seed);
+    static void SetRun (uint32_t run);
+    
+    static ns3::Ptr<ns3::UniformRandomVariable> CreateUniformRandomVariable (double min, double max);
+    
 private:
-  // Private constructor as this is a static class and cannot be instantiated
-  RandomGeneratorManager ();
-
-  static uint32_t m_seed;
-  static uint32_t m_run;
+    RandomGeneratorManager (); // Private constructor as this is a static class and cannot be instantiated
 };
-#endif /* RANDOM_GENERATOR_MANAGER_H */
+#endif /* random_generator_manager_h */
