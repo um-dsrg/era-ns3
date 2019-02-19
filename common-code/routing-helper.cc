b346db471337d0f9d053250ec7f55d6a95ea3337
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-02-19 16:57:27 +0100

    Create a RoutingHelper base class
    The RoutingHelper base class has been created such that we can use polymorphism and use the same pointer irrelevant of the switch pointer used. This is used in the multipath simulation where the switch type is set dynamically at runtime.

--- common-code/routing-helper.cc
+++ common-code/routing-helper.cc
@@ -3,6 +3,58 @@
 NS_LOG_COMPONENT_DEFINE("RoutingHelper");
 
 template <>
+void RoutingHelper<SdnSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &flows,
+                                                  const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {
+    for(const auto &flowPair : flows) {
+
+        const auto &flow{flowPair.second};
+        NS_LOG_INFO ("Installing routing for Flow " << flow.id);
+
+        /* Installing data paths */
+        for (const auto &path : flow.GetDataPaths()) {
+            NS_LOG_INFO ("Installing routing for Data Path " << path.id);
+
+            for (const auto &link : path.GetLinks ()) {
+                NS_LOG_INFO ("Working on Link " << link->id);
+
+                if (link->srcNodeType == NodeType::Switch) {
+                    auto forwardingPort = transmitOnLink.at (link->id);
+                    SdnSwitch *switchNode = dynamic_cast<SdnSwitch*> (link->srcNode);
+                    switchNode->AddEntryToRoutingTable (flow.srcNode->GetIpAddress().Get(),
+                                                        flow.dstNode->GetIpAddress().Get(),
+                                                        path.srcPort, path.dstPort, flow.protocol,
+                                                        forwardingPort);
+                }
+            }
+        }
+
+        if (flow.protocol == FlowProtocol::Tcp) { // Install ACK paths only for TCP flows
+            for (const auto &path : flow.GetAckPaths()) {
+                NS_LOG_INFO ("Installing routing for Ack Path " << path.id);
+
+                for (const auto &link : path.GetLinks ()) {
+                    NS_LOG_INFO ("Working on Link " << link->id);
+
+                    if (link->srcNodeType == NodeType::Switch) {
+                        auto forwardingPort = transmitOnLink.at (link->id);
+                        SdnSwitch *switchNode = dynamic_cast<SdnSwitch*> (link->srcNode);
+
+                        /**
+                         The addresses are reversed becuase the ACK flow has the opposite source and destination
+                         details of the data flow.
+                         */
+                        switchNode->AddEntryToRoutingTable (flow.dstNode->GetIpAddress().Get(),
+                                                            flow.srcNode->GetIpAddress().Get(),
+                                                            path.srcPort, path.dstPort, flow.protocol,
+                                                            forwardingPort);
+                    }
+                }
+            }
+        }
+    }
+}
+
+template <>
 void RoutingHelper<PpfsSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &flows,
                                                    const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {
     NS_LOG_INFO("Building the Routing Table for the PPFS Switch");
--- common-code/routing-helper.h
+++ common-code/routing-helper.h
@@ -10,16 +10,28 @@
 #include "ns3/node-container.h"
 
 #include "flow.h"
+#include "sdn-switch.h"
 #include "definitions.h"
 #include "ppfs-switch.h"
 #include "topology-builder.h"
+
+/**
+ * \brief      Routing helper base class.
+ *
+ * This base class is created to allow the creation of the RoutingHelper class at runtime based on the simulation switch type.
+ */
+class RoutingHelperBase {
+public:
+    virtual void BuildRoutingTable (const std::map<id_t, Flow> &flows, const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) = 0;
+    virtual ~RoutingHelperBase() {}
+};
 
 /**
  RoutingHelper Class Declaration
  */
 
 template <class SwitchType>
-class RoutingHelper {
+class RoutingHelper : public RoutingHelperBase {
     NS_LOG_TEMPLATE_DECLARE; /**< Logging component. */
 
 public:
@@ -35,57 +47,4 @@ public:
 template <class SwitchType>
 RoutingHelper<SwitchType>::RoutingHelper () : NS_LOG_TEMPLATE_DEFINE ("RoutingHelper") {
 }
-
-template <class SwitchType>
-void RoutingHelper<SwitchType>::BuildRoutingTable (const std::map<id_t, Flow> &flows,
-                                                   const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {
-    for(const auto &flowPair : flows) {
-
-        const auto &flow{flowPair.second};
-        NS_LOG_INFO ("Installing routing for Flow " << flow.id);
-
-        /* Installing data paths */
-        for (const auto &path : flow.GetDataPaths()) {
-            NS_LOG_INFO ("Installing routing for Data Path " << path.id);
-
-            for (const auto &link : path.GetLinks ()) {
-                NS_LOG_INFO ("Working on Link " << link->id);
-
-                if (link->srcNodeType == NodeType::Switch) {
-                    auto forwardingPort = transmitOnLink.at (link->id);
-                    SwitchType *switchNode = dynamic_cast<SwitchType *> (link->srcNode);
-                    switchNode->AddEntryToRoutingTable (flow.srcNode->GetIpAddress().Get(),
-                                                        flow.dstNode->GetIpAddress().Get(),
-                                                        path.srcPort, path.dstPort, flow.protocol,
-                                                        forwardingPort);
-                }
-            }
-        }
-
-        if (flow.protocol == FlowProtocol::Tcp) { // Install ACK paths only for TCP flows
-            for (const auto &path : flow.GetAckPaths()) {
-                NS_LOG_INFO ("Installing routing for Ack Path " << path.id);
-
-                for (const auto &link : path.GetLinks ()) {
-                    NS_LOG_INFO ("Working on Link " << link->id);
-
-                    if (link->srcNodeType == NodeType::Switch) {
-                        auto forwardingPort = transmitOnLink.at (link->id);
-                        SwitchType *switchNode = dynamic_cast<SwitchType *> (link->srcNode);
-
-                        /**
-                         The addresses are reversed becuase the ACK flow has the opposite source and destination
-                         details of the data flow.
-                         */
-                        switchNode->AddEntryToRoutingTable (flow.dstNode->GetIpAddress().Get(),
-                                                            flow.srcNode->GetIpAddress().Get(),
-                                                            path.srcPort, path.dstPort, flow.protocol,
-                                                            forwardingPort);
-                    }
-                }
-            }
-        }
-    }
-}
-
 #endif /* routing_helper_h */
