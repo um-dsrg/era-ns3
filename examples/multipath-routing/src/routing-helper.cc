abfcf43c1af6a9aa4b192337f27b92e7f0da9da0
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-03-26 14:20:55 +0100

    Remove the RoutingHelper class and set it equal to one function

--- examples/multipath-routing/src/routing-helper.cc
+++ examples/multipath-routing/src/routing-helper.cc
@@ -1,59 +1,81 @@
+#include "ns3/log.h"
+
 #include "routing-helper.h"
+#include "device/switch/switch-base.h"
+
+using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("RoutingHelper");
 
-template <>
-void RoutingHelper<SdnSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &flows,
-                                                  const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {
-    NS_LOG_INFO("Building the Routing Table for the SDN Switches");
+void
+BuildRoutingTable (const std::map<id_t, Flow> &flows,
+                   const std::map<id_t, Ptr<NetDevice>> &transmitOnLink)
+{
+  NS_LOG_INFO ("Building the Routing Table");
 
-    for(const auto &flowPair : flows) {
-
-        const auto &flow{flowPair.second};
-        if (flow.dataRate == 0) {
-            NS_LOG_INFO ("Flow " << flow.id << " is not being installed in the routing tables because it was allocated nothing");
-            continue;
+  for (const auto &flowPair : flows)
+    {
+      const auto &flow{flowPair.second};
+      if (flow.dataRate == 0)
+        {
+          NS_LOG_INFO ("Flow " << flow.id
+                               << " is not being installed in the routing tables because it is "
+                                  "allocated no data rate");
+          continue;
         }
 
-        NS_LOG_INFO ("Installing routing for Flow " << flow.id);
+      NS_LOG_INFO ("Installing routing for Flow " << flow.id);
 
-        /* Installing data paths */
-        for (const auto &path : flow.GetDataPaths()) {
-            NS_LOG_INFO ("Installing routing for Data Path " << path.id);
+      // TODO move this to a function
+      /* Installing data paths */
+      auto flowDataRate = double{static_cast<double> (flow.dataRate.GetBitRate ())};
+      for (const auto &path : flow.GetDataPaths ())
+        {
+          auto pathSplitRatio = double{path.dataRate.GetBitRate () / flowDataRate};
+          NS_LOG_INFO ("Installing routing for Data Path " << path.id << ". "
+                                                           << "Split Ratio: " << pathSplitRatio);
 
-            for (const auto &link : path.GetLinks ()) {
-                NS_LOG_INFO ("Working on Link " << link->id);
+          for (const auto &link : path.GetLinks ())
+            {
+              NS_LOG_INFO ("Working on Link " << link->id);
 
-                if (link->srcNodeType == NodeType::Switch) {
-                    auto forwardingPort = transmitOnLink.at (link->id);
-                    SdnSwitch *switchNode = dynamic_cast<SdnSwitch*> (link->srcNode);
-                    switchNode->AddEntryToRoutingTable (flow.srcNode->GetIpAddress().Get(),
-                                                        flow.dstNode->GetIpAddress().Get(),
-                                                        path.srcPort, path.dstPort, flow.protocol,
-                                                        forwardingPort);
+              if (link->srcNodeType == NodeType::Switch)
+                {
+                  auto forwardingPort = transmitOnLink.at (link->id);
+                  auto *switchNode = dynamic_cast<SwitchBase *> (link->srcNode);
+                  switchNode->AddEntryToRoutingTable (
+                      flow.srcNode->GetIpAddress ().Get (), flow.dstNode->GetIpAddress ().Get (),
+                      path.srcPort, path.dstPort, flow.protocol, forwardingPort, pathSplitRatio);
                 }
             }
         }
 
-        if (flow.protocol == FlowProtocol::Tcp) { // Install ACK paths only for TCP flows
-            for (const auto &path : flow.GetAckPaths()) {
-                NS_LOG_INFO ("Installing routing for Ack Path " << path.id);
+      /* Installing the ACK paths for TCP flows only */
+      // TODO Update this to be handled equally well for PPFS and TCP!
+      // Currently it only works for SDN and move this to a function
+      if (flow.protocol == FlowProtocol::Tcp)
+        {
+          for (const auto &path : flow.GetAckPaths ())
+            {
+              NS_LOG_INFO ("Installing routing for Ack Path " << path.id);
 
-                for (const auto &link : path.GetLinks ()) {
-                    NS_LOG_INFO ("Working on Link " << link->id);
+              for (const auto &link : path.GetLinks ())
+                {
+                  NS_LOG_INFO ("Working on Link " << link->id);
 
-                    if (link->srcNodeType == NodeType::Switch) {
-                        auto forwardingPort = transmitOnLink.at (link->id);
-                        SdnSwitch *switchNode = dynamic_cast<SdnSwitch*> (link->srcNode);
+                  if (link->srcNodeType == NodeType::Switch)
+                    {
+                      auto forwardingPort = transmitOnLink.at (link->id);
+                      auto *switchNode = dynamic_cast<SwitchBase *> (link->srcNode);
 
-                        /**
-                         The addresses are reversed becuase the ACK flow has the opposite source and destination
-                         details of the data flow.
+                      /**
+                         The addresses are reversed becuase the ACK flow has the opposite source
+                         and destination details of the data flow.
                          */
-                        switchNode->AddEntryToRoutingTable (flow.dstNode->GetIpAddress().Get(),
-                                                            flow.srcNode->GetIpAddress().Get(),
-                                                            path.srcPort, path.dstPort, flow.protocol,
-                                                            forwardingPort);
+                      switchNode->AddEntryToRoutingTable (
+                          flow.dstNode->GetIpAddress ().Get (),
+                          flow.srcNode->GetIpAddress ().Get (), path.srcPort, path.dstPort,
+                          flow.protocol, forwardingPort, 1.0 /* Ack flows are not split*/);
                     }
                 }
             }
@@ -61,66 +83,125 @@ void RoutingHelper<SdnSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &fl
     }
 }
 
-template <>
-void RoutingHelper<PpfsSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &flows,
-                                                   const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {
-    NS_LOG_INFO("Building the Routing Table for the PPFS Switches");
+/*template <>*/
+/*void RoutingHelper<SdnSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &flows,*/
+/*                                                  const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {*/
+/*    NS_LOG_INFO("Building the Routing Table for the SDN Switches");*/
 
-    for(const auto &flowPair : flows) {
+/*    for(const auto &flowPair : flows) {*/
 
-        const auto &flow{flowPair.second};
-        if (flow.dataRate == 0) {
-            NS_LOG_INFO ("Flow " << flow.id << " is not being installed in the routing tables because it was allocated nothing");
-            continue;
-        }
-        NS_LOG_INFO ("Installing routing for Flow " << flow.id);
+/*        const auto &flow{flowPair.second};*/
+/*        if (flow.dataRate == 0) {*/
+/*            NS_LOG_INFO ("Flow " << flow.id << " is not being installed in the routing tables because it was allocated nothing");*/
+/*            continue;*/
+/*        }*/
 
-        auto flowDataRate = double{static_cast<double>(flow.dataRate.GetBitRate())};
+/*        NS_LOG_INFO ("Installing routing for Flow " << flow.id);*/
 
-        /* Installing data paths */
-        for (const auto &path : flow.GetDataPaths()) {
-            auto pathSplitRatio = double{path.dataRate.GetBitRate() / flowDataRate};
-            NS_LOG_INFO ("Installing routing for Data Path " << path.id << ". " <<
-                         "Split Ratio: " << pathSplitRatio);
+/* Installing data paths */
+/*        for (const auto &path : flow.GetDataPaths()) {*/
+/*            NS_LOG_INFO ("Installing routing for Data Path " << path.id);*/
 
-            for (const auto &link : path.GetLinks ()) {
-                NS_LOG_INFO ("Working on Link " << link->id);
+/*            for (const auto &link : path.GetLinks ()) {*/
+/*                NS_LOG_INFO ("Working on Link " << link->id);*/
 
-                if (link->srcNodeType == NodeType::Switch) {
-                    auto forwardingPort = transmitOnLink.at (link->id);
-                    PpfsSwitch* switchNode = dynamic_cast<PpfsSwitch*>(link->srcNode);
-                    switchNode->AddEntryToRoutingTable(flow.srcNode->GetIpAddress().Get(),
-                                                       flow.dstNode->GetIpAddress().Get(),
-                                                       path.srcPort, path.dstPort, flow.protocol,
-                                                       pathSplitRatio, forwardingPort);
-                }
-            }
-        }
+/*                if (link->srcNodeType == NodeType::Switch) {*/
+/*                    auto forwardingPort = transmitOnLink.at (link->id);*/
+/*                    SdnSwitch *switchNode = dynamic_cast<SdnSwitch*> (link->srcNode);*/
+/*                    switchNode->AddEntryToRoutingTable (flow.srcNode->GetIpAddress().Get(),*/
+/*                                                        flow.dstNode->GetIpAddress().Get(),*/
+/*                                                        path.srcPort, path.dstPort, flow.protocol,*/
+/*                                                        forwardingPort);*/
+/*                }*/
+/*            }*/
+/*        }*/
 
-        if (flow.protocol == FlowProtocol::Tcp) { // Install ACK paths only for TCP flows
-            const auto& ackShortestPath = flow.GetAckShortestPath();
-            NS_LOG_INFO ("Installing routing for the Ack Shortest Path");
+/*        if (flow.protocol == FlowProtocol::Tcp) { // Install ACK paths only for TCP flows*/
+/*            for (const auto &path : flow.GetAckPaths()) {*/
+/*                NS_LOG_INFO ("Installing routing for Ack Path " << path.id);*/
 
-            for (const auto &link : ackShortestPath.GetLinks ()) {
-                NS_LOG_INFO ("Working on Link " << link->id);
+/*                for (const auto &link : path.GetLinks ()) {*/
+/*                    NS_LOG_INFO ("Working on Link " << link->id);*/
 
-                if (link->srcNodeType == NodeType::Switch) {
-                    auto forwardingPort = transmitOnLink.at (link->id);
-                    PpfsSwitch* switchNode = dynamic_cast<PpfsSwitch*>(link->srcNode);
+/*                    if (link->srcNodeType == NodeType::Switch) {*/
+/*                        auto forwardingPort = transmitOnLink.at (link->id);*/
+/*                        SdnSwitch *switchNode = dynamic_cast<SdnSwitch*> (link->srcNode);*/
 
-                    /**
-                     The addresses are reversed becuase the ACK flow has the opposite source and destination
-                     details of the data flow.
-                     */
-                    switchNode->AddEntryToRoutingTable(flow.dstNode->GetIpAddress().Get(),
-                                                       flow.srcNode->GetIpAddress().Get(),
-                                                       ackShortestPath.srcPort,
-                                                       ackShortestPath.dstPort,
-                                                       flow.protocol,
-                                                       1.0, /* Ack flows are not split */
-                                                       forwardingPort);
-                }
-            }
-        }
-    }
-}
+/***/
+/*                         The addresses are reversed becuase the ACK flow has the opposite source and destination*/
+/*                         details of the data flow.*/
+/*                         */
+/*                        switchNode->AddEntryToRoutingTable (flow.dstNode->GetIpAddress().Get(),*/
+/*                                                            flow.srcNode->GetIpAddress().Get(),*/
+/*                                                            path.srcPort, path.dstPort, flow.protocol,*/
+/*                                                            forwardingPort);*/
+/*                    }*/
+/*                }*/
+/*            }*/
+/*        }*/
+/*    }*/
+/*}*/
+
+/*template <>*/
+/*void RoutingHelper<PpfsSwitch>::BuildRoutingTable (const std::map<id_t, Flow> &flows,*/
+/*                                                   const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) {*/
+/*    NS_LOG_INFO("Building the Routing Table for the PPFS Switches");*/
+
+/*    for(const auto &flowPair : flows) {*/
+
+/*        const auto &flow{flowPair.second};*/
+/*        if (flow.dataRate == 0) {*/
+/*            NS_LOG_INFO ("Flow " << flow.id << " is not being installed in the routing tables because it was allocated nothing");*/
+/*            continue;*/
+/*        }*/
+/*        NS_LOG_INFO ("Installing routing for Flow " << flow.id);*/
+
+/*        auto flowDataRate = double{static_cast<double>(flow.dataRate.GetBitRate())};*/
+
+/* Installing data paths */
+/*        for (const auto &path : flow.GetDataPaths()) {*/
+/*            auto pathSplitRatio = double{path.dataRate.GetBitRate() / flowDataRate};*/
+/*            NS_LOG_INFO ("Installing routing for Data Path " << path.id << ". " <<*/
+/*                         "Split Ratio: " << pathSplitRatio);*/
+
+/*            for (const auto &link : path.GetLinks ()) {*/
+/*                NS_LOG_INFO ("Working on Link " << link->id);*/
+
+/*                if (link->srcNodeType == NodeType::Switch) {*/
+/*                    auto forwardingPort = transmitOnLink.at (link->id);*/
+/*                    PpfsSwitch* switchNode = dynamic_cast<PpfsSwitch*>(link->srcNode);*/
+/*                    switchNode->AddEntryToRoutingTable(flow.srcNode->GetIpAddress().Get(),*/
+/*                                                       flow.dstNode->GetIpAddress().Get(),*/
+/*                                                       path.srcPort, path.dstPort, flow.protocol,*/
+/*                                                       pathSplitRatio, forwardingPort);*/
+/*                }*/
+/*            }*/
+/*        }*/
+
+/*        if (flow.protocol == FlowProtocol::Tcp) { // Install ACK paths only for TCP flows*/
+/*            const auto& ackShortestPath = flow.GetAckShortestPath();*/
+/*            NS_LOG_INFO ("Installing routing for the Ack Shortest Path");*/
+
+/*            for (const auto &link : ackShortestPath.GetLinks ()) {*/
+/*                NS_LOG_INFO ("Working on Link " << link->id);*/
+
+/*                if (link->srcNodeType == NodeType::Switch) {*/
+/*                    auto forwardingPort = transmitOnLink.at (link->id);*/
+/*                    PpfsSwitch* switchNode = dynamic_cast<PpfsSwitch*>(link->srcNode);*/
+
+/***/
+/*                     The addresses are reversed becuase the ACK flow has the opposite source and destination*/
+/*                     details of the data flow.*/
+/*                     */
+/*                    switchNode->AddEntryToRoutingTable(flow.dstNode->GetIpAddress().Get(),*/
+/*                                                       flow.srcNode->GetIpAddress().Get(),*/
+/*                                                       ackShortestPath.srcPort,*/
+/*                                                       ackShortestPath.dstPort,*/
+/*                                                       flow.protocol,*/
+/*                                                       1.0, Ack flows are not split */
+/*                                                       forwardingPort);*/
+/*                }*/
+/*            }*/
+/*        }*/
+/*    }*/
+/*}*/
--- examples/multipath-routing/src/routing-helper.h
+++ examples/multipath-routing/src/routing-helper.h
@@ -1,50 +1,10 @@
 #ifndef routing_helper_h
 #define routing_helper_h
 
-#include <map>
-#include <tinyxml2.h>
-
-#include "ns3/node.h"
-#include "ns3/packet.h"
 #include "ns3/net-device.h"
-#include "ns3/node-container.h"
 
 #include "flow.h"
-#include "device/switch/sdn-switch.h"
-#include "definitions.h"
-#include "device/switch/ppfs-switch.h"
-#include "topology-builder.h"
 
-/**
- * \brief      Routing helper base class.
- *
- * This base class is created to allow the creation of the RoutingHelper class at runtime based on the simulation switch type.
- */
-class RoutingHelperBase {
-public:
-    virtual void BuildRoutingTable (const std::map<id_t, Flow> &flows, const std::map<id_t, Ptr<NetDevice>> &transmitOnLink) = 0;
-    virtual ~RoutingHelperBase() {}
-};
-
-/**
- RoutingHelper Class Declaration
- */
-
-template <class SwitchType>
-class RoutingHelper : public RoutingHelperBase {
-    NS_LOG_TEMPLATE_DECLARE; /**< Logging component. */
-
-public:
-    RoutingHelper ();
-    void BuildRoutingTable (const std::map<id_t, Flow> &flows,
-                            const std::map<id_t, Ptr<NetDevice>> &transmitOnLink);
-};
-
-/**
- RoutingHelper Class Implementation
- */
-
-template <class SwitchType>
-RoutingHelper<SwitchType>::RoutingHelper () : NS_LOG_TEMPLATE_DEFINE ("RoutingHelper") {
-}
+void BuildRoutingTable (const std::map<id_t, Flow> &flows,
+                        const std::map<id_t, ns3::Ptr<ns3::NetDevice>> &transmitOnLink);
 #endif /* routing_helper_h */
