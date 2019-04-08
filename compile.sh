e042eb26b7385fc1d3532d28f9fd479aaaf6f2d1
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-04-08 09:34:05 +0200

    Add the compile script to handle MacOs and Linux

--- compile.sh
+++ compile.sh
@@ -2,13 +2,21 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+DEBUG_CONFIG_COMMAND="./waf configure --enable-examples --build-profile=debug --out=${SCRIPT_DIR}/build/debug"
+OPT_CONFIG_COMMAND="./waf configure --enable-examples --build-profile=optimized --out=${SCRIPT_DIR}/build/optimized"
+
+if [ "$(uname)" == "Darwin" ]; then
+    DEBUG_CONFIG_COMMAND="${DEBUG_CONFIG_COMMAND} --disable-werror"
+    OPT_CONFIG_COMMAND="${OPT_CONFIG_COMMAND} --disable-werror"
+fi
+
 function buildDebug {
-    cd ${SCRIPT_DIR} && ./waf configure --enable-examples --build-profile=debug --out=${SCRIPT_DIR}/build/debug
+    cd ${SCRIPT_DIR} && ${DEBUG_CONFIG_COMMAND}
     cd ${SCRIPT_DIR} && ./waf build
 }
 
 function buildOptimized {
-    cd ${SCRIPT_DIR} && ./waf configure --enable-examples --build-profile=optimized --out=${SCRIPT_DIR}/build/optimized
+    cd ${SCRIPT_DIR} && ${OPT_CONFIG_COMMAND}
     cd ${SCRIPT_DIR} && ./waf build
 }
 
