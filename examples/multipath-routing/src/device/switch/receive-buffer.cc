31eb0fc9168865d0a644643d17586948a3c4b5f4
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-08-01 17:15:04 +0200

    Add the implementation of the ReceiveBuffer class

---
+++ examples/multipath-routing/src/device/switch/receive-buffer.cc
@@ -0,0 +1,54 @@
+#include "ns3/log.h"
+#include "ns3/simulator.h"
+
+#include "receive-buffer.h"
+
+NS_LOG_COMPONENT_DEFINE ("TransmitBuffer");
+
+using namespace ns3;
+
+ReceiveBuffer::ReceiveBuffer(id_t switchId, uint64_t bufferSize) :
+  m_switchId(switchId), m_bufferSize(bufferSize)
+{}
+
+bool
+ReceiveBuffer::AddPacket(Ptr<const Packet> packet)
+{
+  auto packetSize {packet->GetSize()};
+
+  if ((packetSize + m_usedCapacity) <= m_bufferSize)
+  {
+    m_usedCapacity += packetSize;
+
+    NS_ABORT_MSG_IF(m_usedCapacity > m_bufferSize, "Switch " << m_switchId << " used capacity " <<
+                    "exceeded buffer size. Used capacity: " << m_usedCapacity <<
+                    " Buffer size: " << m_bufferSize);
+
+    NS_LOG_INFO(Simulator::Now().GetSeconds() << "s: Switch: " << m_switchId << " packet ADDED to "
+                "receive buffer. Packet size: " << packetSize << " Used capacity: " <<
+                m_usedCapacity);
+    return true;
+  }
+  else
+  {
+    m_numDroppedPackets++;
+    NS_LOG_INFO(Simulator::Now().GetSeconds() << "s: Switch: " << m_switchId << " packet DROPPED at"
+                "receive buffer. Packet size: " << packetSize << " Used capacity: " <<
+                m_usedCapacity << " Num dropped packets: " << m_numDroppedPackets);
+    return false;
+  }
+}
+
+void
+ReceiveBuffer::RemovePacket(Ptr<const Packet> packet)
+{
+  auto packetSize {packet->GetSize()};
+  m_usedCapacity -= packetSize;
+
+  NS_ABORT_MSG_IF(m_usedCapacity < 0, "Switch " << m_switchId << " negative used capacity. " <<
+                  "Value: " << m_usedCapacity);
+
+  NS_LOG_INFO(Simulator::Now().GetSeconds() << "s: Switch: " << m_switchId << " packet REMOVED " <<
+              "from receive buffer. Packet size: " << packetSize << " Used capacity: " <<
+              m_usedCapacity);
+}
---
+++ examples/multipath-routing/src/device/switch/receive-buffer.h
@@ -0,0 +1,25 @@
+#ifndef receive_buffer_h
+#define receive_buffer_h
+
+#include "ns3/packet.h"
+
+#include "../../definitions.h"
+
+class ReceiveBuffer
+{
+public:
+  ReceiveBuffer(id_t switchId, uint64_t bufferSize);
+
+  bool AddPacket(ns3::Ptr<const ns3::Packet> packet);
+  void RemovePacket(ns3::Ptr<const ns3::Packet> packet);
+
+private:
+  const id_t m_switchId;
+
+  /* Buffer related variables */
+  uint64_t m_usedCapacity;
+  const uint64_t m_bufferSize;
+  uint64_t m_numDroppedPackets = 0;
+};
+
+#endif /* receive_buffer_h */
