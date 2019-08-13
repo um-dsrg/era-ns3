bee40ca260886f9f3be348a57d7e4352f3263bc9
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-08-13 11:36:27 +0200

    Implement the Enqueue & GetPacketType function for the Transmit Queue

--- examples/multipath-routing/src/device/switch/transmit-queue.cc
+++ examples/multipath-routing/src/device/switch/transmit-queue.cc
@@ -1,10 +1,61 @@
+#include "ns3/simulator.h"
+#include "ns3/udp-header.h"
+#include "ns3/tcp-header.h"
+#include "ns3/ppp-header.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/tcp-l4-protocol.h"
+#include "ns3/udp-l4-protocol.h"
+#include "ns3/ipv4-l3-protocol.h"
+#include "ns3/icmpv4-l4-protocol.h"
+
 #include "transmit-queue.h"
+
+NS_LOG_COMPONENT_DEFINE ("TransmitQueue");
+
+using namespace ns3;
+
+TransmitQueue::TransmitQueue () : NS_LOG_TEMPLATE_DEFINE ("TransmitQueue")
+{
+}
 
 bool
 TransmitQueue::Enqueue (ns3::Ptr<ns3::Packet> packet)
 {
-  std::cout << "A packet is received" << std::endl;
-  m_dataQueue.push (packet);
+  if (m_retrievalMethod == RetrievalMethod::InOrder)
+    {
+      m_dataQueue.push (packet);
+      NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+                   << "s - Switch " << m_switchId
+                   << ": Add packet to DATA transmit buffer. Packets in DATA buffer: "
+                   << m_dataQueue.size () << " Packets in ACK buffer: " << m_ackQueue.size ());
+    }
+  else
+    {
+      auto packetType = GetPacketType (packet);
+
+      if (packetType == PacketType::Data)
+        {
+          m_dataQueue.push (packet);
+          NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+                       << "s - Switch " << m_switchId
+                       << ": Add packet to DATA transmit buffer. Packets in DATA buffer: "
+                       << m_dataQueue.size () << " Packets in ACK buffer: " << m_ackQueue.size ());
+        }
+      else if (packetType == PacketType::Ack)
+        {
+          m_ackQueue.push (packet);
+          NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+                       << "s - Switch " << m_switchId
+                       << ": Add packet to ACK transmit buffer. Packets in DATA buffer: "
+                       << m_dataQueue.size () << " Packets in ACK buffer: " << m_ackQueue.size ());
+        }
+      else
+        {
+          NS_ABORT_MSG ("Switch " << m_switchId << "Adding packet to transmit buffer failed.");
+          return false;
+        }
+    }
+
   return true;
 }
 
@@ -22,13 +73,62 @@ TransmitQueue::Dequeue ()
 ns3::Ptr<ns3::Packet>
 TransmitQueue::Remove ()
 {
-  std::cout << "Calling the remove function" << std::endl;
-  return nullptr;
+  NS_ABORT_MSG ("TransmitQueue::Remove is not implemented and should not be used");
+  return 0;
 }
 
 ns3::Ptr<const ns3::Packet>
 TransmitQueue::Peek () const
 {
-  std::cout << "Calling the peek function" << std::endl;
-  return nullptr;
+  NS_ABORT_MSG ("TransmitQueue::Peek is not implemented and should not be used");
+  return 0;
+}
+
+PacketType
+TransmitQueue::GetPacketType (ns3::Ptr<const ns3::Packet> packet) const
+{
+  PacketType packetType{PacketType::Data};
+  Ptr<Packet> receivedPacket = packet->Copy (); // Copy the packet for parsing purposes
+
+  // Remove the PPP header
+  PppHeader pppHeader;
+  if (receivedPacket->PeekHeader (pppHeader))
+    {
+      receivedPacket->RemoveHeader (pppHeader);
+
+      Ipv4Header ipHeader;
+      uint8_t ipProtocol{0};
+
+      if (receivedPacket->PeekHeader (ipHeader))
+        { // Extract the transport layer protocol (UDP/TCP)
+          ipProtocol = ipHeader.GetProtocol ();
+          receivedPacket->RemoveHeader (ipHeader);
+
+          if (ipProtocol == TcpL4Protocol::PROT_NUMBER) // TCP Packet
+            {
+              TcpHeader tcpHeader;
+              uint8_t tcpFlags{0};
+
+              if (receivedPacket->PeekHeader (tcpHeader))
+                {
+                  tcpFlags = tcpHeader.GetFlags ();
+                  receivedPacket->RemoveHeader (tcpHeader);
+                }
+
+              // If ACK flag is set AND no data carried by packet, this is an ACK packet
+              if ((tcpFlags & (1 << 5)) && (receivedPacket->GetSize () == 0))
+                packetType = PacketType::Ack;
+            }
+        }
+      else
+        {
+          NS_ABORT_MSG ("Non-IP Packet received");
+        }
+    }
+  else
+    {
+      NS_ABORT_MSG ("Non P2P Packet received");
+    }
+
+  return packetType;
 }
--- examples/multipath-routing/src/device/switch/transmit-queue.h
+++ examples/multipath-routing/src/device/switch/transmit-queue.h
@@ -3,19 +3,33 @@
 
 #include <queue>
 
+#include "ns3/log.h"
 #include "ns3/queue.h"
 #include "ns3/packet.h"
+
+#include "../../definitions.h"
 
 class TransmitQueue : public ns3::Queue<ns3::Packet>
 {
 public:
-  bool Enqueue (ns3::Ptr<ns3::Packet> packet);
-  ns3::Ptr<ns3::Packet> Dequeue ();
-  ns3::Ptr<ns3::Packet> Remove ();
-  ns3::Ptr<const ns3::Packet> Peek () const;
+  TransmitQueue ();
+
+  bool Enqueue (ns3::Ptr<ns3::Packet> packet) override;
+  ns3::Ptr<ns3::Packet> Dequeue () override;
+  ns3::Ptr<ns3::Packet> Remove () override;
+  ns3::Ptr<const ns3::Packet> Peek () const override;
 
 private:
+  enum class RetrievalMethod { RoundRobin, AckPriority, InOrder };
+
+  PacketType GetPacketType (ns3::Ptr<const ns3::Packet> packet) const;
+
+  id_t m_switchId;
+  RetrievalMethod m_retrievalMethod;
+  std::queue<ns3::Ptr<ns3::Packet>> m_ackQueue;
   std::queue<ns3::Ptr<ns3::Packet>> m_dataQueue;
+
+  ns3::NS_LOG_TEMPLATE_DECLARE;
 };
 
 #endif /* transmit_queue_h */
