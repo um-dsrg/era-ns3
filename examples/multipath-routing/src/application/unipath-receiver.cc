30cbdc25c22a859a380599ce82250c2211e35c3f
Noel Farrugia <noel.farrugia.09@um.edu.mt>
2019-08-30 15:28:30 +0200

    Refactor the unipath-receiver.cc file to match ns3 code style

--- examples/multipath-routing/src/application/unipath-receiver.cc
+++ examples/multipath-routing/src/application/unipath-receiver.cc
@@ -19,15 +19,15 @@ UnipathReceiver::UnipathReceiver (const Flow &flow, ResultsContainer &resContain
   dstAddress = Address (InetSocketAddress (flow.dstNode->GetIpAddress (), path.dstPort));
 
   if (flow.protocol == FlowProtocol::Tcp)
-  {
-    auto tcpBufferSize = CalculateTcpBufferSize(flow);
-    NS_LOG_INFO("UnipathReceiver - Flow: " << flow.id << " calculated TCP buffer size: " <<
-                tcpBufferSize << "bytes");
+    {
+      auto tcpBufferSize = CalculateTcpBufferSize (flow);
+      NS_LOG_INFO ("UnipathReceiver - Flow: " << flow.id << " calculated TCP buffer size: "
+                                              << tcpBufferSize << "bytes");
 
-    auto tcpSocket = ns3::DynamicCast<ns3::TcpSocket> (rxListenSocket);
-    tcpSocket->SetAttribute("SndBufSize", ns3::UintegerValue(tcpBufferSize));
-    tcpSocket->SetAttribute("RcvBufSize", ns3::UintegerValue(tcpBufferSize));
-  }
+      auto tcpSocket = ns3::DynamicCast<ns3::TcpSocket> (rxListenSocket);
+      tcpSocket->SetAttribute ("SndBufSize", ns3::UintegerValue (tcpBufferSize));
+      tcpSocket->SetAttribute ("RcvBufSize", ns3::UintegerValue (tcpBufferSize));
+    }
 
   m_dataPacketSize = flow.packetSize - CalculateHeaderSize (flow.protocol);
   NS_LOG_INFO ("Flow " << flow.id << "\nPacket size including headers: " << flow.packetSize
@@ -47,8 +47,8 @@ UnipathReceiver::~UnipathReceiver ()
 void
 UnipathReceiver::StartApplication ()
 {
-  NS_LOG_INFO (Simulator::Now().GetSeconds() << "s - UnipathReceiver: Flow " << m_id <<
-               " started reception");
+  NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+               << "s - UnipathReceiver: Flow " << m_id << " started reception");
 
   // Initialise socket connections
   if (rxListenSocket->Bind (dstAddress) == -1)
@@ -70,8 +70,8 @@ UnipathReceiver::StartApplication ()
 void
 UnipathReceiver::StopApplication ()
 {
-  NS_LOG_INFO (Simulator::Now().GetSeconds() << "s - UnipathReceiver: Flow " << m_id <<
-               " stopped reception");
+  NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+               << "s - UnipathReceiver: Flow " << m_id << " stopped reception");
 }
 
 void
@@ -95,15 +95,15 @@ UnipathReceiver::HandleRead (Ptr<Socket> socket)
         }
 
       auto recvBytes = packet->GetSize ();
-      NS_LOG_INFO (Simulator::Now ().GetSeconds () << "s - Flow " << m_id << " received " <<
-                   recvBytes << "bytes of data");
+      NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+                   << "s - Flow " << m_id << " received " << recvBytes << "bytes of data");
 
       pendingBytes += recvBytes;
 
       // The floor and division should be here
       auto packetsReceived = floor (pendingBytes / static_cast<double> (m_dataPacketSize));
-      NS_LOG_INFO (Simulator::Now ().GetSeconds () << "s - Flow " << m_id << " extracted " <<
-                   packetsReceived << " packet(s)");
+      NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+                   << "s - Flow " << m_id << " extracted " << packetsReceived << " packet(s)");
 
       // Log for every data packet size, packet received
       for (auto i = 0; i < packetsReceived; ++i)
@@ -115,7 +115,8 @@ UnipathReceiver::HandleRead (Ptr<Socket> socket)
 
       // Subtract the number of bytes that have been logged already
       pendingBytes -= packetsReceived * m_dataPacketSize;
-      NS_LOG_INFO (Simulator::Now().GetSeconds() << "s - Flow " << m_id << " Number of pending bytes remaining: " <<
-                   pendingBytes);
+      NS_LOG_INFO (Simulator::Now ().GetSeconds ()
+                   << "s - Flow " << m_id
+                   << " Number of pending bytes remaining: " << pendingBytes);
     }
 }
